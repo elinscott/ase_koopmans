d72e51e72166eb320575df8a9e046cb78d1d03bf
Asmus O. Dohn <asod@hi.is>
2018-01-16 12:04:16 +0000

    cleanup of tests

--- ase/test/combine_mm2.py
+++ ase/test/combine_mm2.py
@@ -2,12 +2,10 @@ from __future__ import print_function
 from ase.data import s22
 from ase.optimize.bfgs import BFGS
 from ase.constraints import FixBondLengths
-from ase.calculators.tip3p import TIP3P, rOH, angleHOH, epsilon0, sigma0
-from ase.calculators.qmmm import LJInteractionsGeneral, LJInteractions
+from ase.calculators.tip3p import TIP3P, epsilon0, sigma0
+from ase.calculators.qmmm import LJInteractionsGeneral
 from ase.calculators.combine_mm import CombineMM
 import numpy as np
-from ase.visualize import view
-from ase.io.trajectory import TrajectoryWriter
 
 def make_atoms():
     atoms = s22.create_s22_system('Water_dimer')
