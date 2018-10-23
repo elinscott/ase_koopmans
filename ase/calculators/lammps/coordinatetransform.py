3b24c32ec9061df06510569f5d8d06083304ec88
Morten Nagel <morten.nagel@helsinki.fi>
2018-10-23 17:24:39 +0300

    LAMMPS: edge case handling for coordination transformation for too tilted cells

--- ase/calculators/lammps/coordinatetransform.py
+++ ase/calculators/lammps/coordinatetransform.py
@@ -1,4 +1,5 @@
 import numpy as np
+from ase.geometry import wrap_positions
 
 
 class Prism:
@@ -13,20 +14,41 @@ class Prism:
     :param tolerance: precision for skewness test
     """
 
-    def __init__(self, cell, pbc=(True, True, True), tolerance=1.0e-6):
+    # !TODO: derive tolerence from cell-dimensions
+    def __init__(self, cell, pbc=(True, True, True), tolerance=1.0e-8):
         # Use LQ decomposition to get the lammps cell
         # ase_cell * R = lammps_cell
-        qtrans, ltrans = np.linalg.qr(cell.T)
+        qtrans, ltrans = np.linalg.qr(cell.T, mode='complete')
         self.R = qtrans
         self.lammps_cell = ltrans.T
         self.tolerance = tolerance
+        self.pbc = pbc
 
-        if self.is_skewed() and not np.all(pbc):
-            raise RuntimeError('Skewed lammps cells MUST have '
-                               'PBC == True in all directions!')
+        lc = self.lammps_cell
+
+        # lammps requires positve values on the diagonal of the
+        # triangluar matrix -> mirror if necessary
+        for i in range(3):
+            if lc[i][i] < 0.0:
+                mirror_mat = np.eye(3)
+                mirror_mat[i][i] = -1.0
+                self.lammps_cell = np.dot(mirror_mat, self.lammps_cell.T).T
+                self.R = np.dot(self.R, mirror_mat)
+
+        # lammps minimizes the edge length of the parallelepiped
+        self.flip = np.array([abs(lc[1][0]/lc[0][0]) > 0.5 and self.pbc[0],
+                              abs(lc[2][0]/lc[0][0]) > 0.5 and self.pbc[0],
+                              abs(lc[2][1]/lc[1][1]) > 0.5 and self.pbc[1],
+                              ])
+        if self.flip[0]:
+            lc[1][0] -= np.sign(lc[1][0])*lc[0][0]
+        if self.flip[1]:
+            lc[2][0] -= np.sign(lc[2][0])*lc[0][0]
+        if self.flip[2]:
+            lc[2][1] -= np.sign(lc[2][1])*lc[1][1]
 
     def get_lammps_prism(self):
-        """Return lammps cell
+        """Return into lammps coordination system rotated cell
 
         :returns: lammps cell
         :rtype: np.array
@@ -34,6 +56,7 @@ class Prism:
         """
         return self.lammps_cell[(0, 1, 2, 1, 2, 2), (0, 1, 2, 0, 0, 1)]
 
+    # !TODO: detect flip in lammps
     def update_cell(self, xyz, offdiag):
         """Rotate new lammps cell into ase coordinate system
 
@@ -44,7 +67,17 @@ class Prism:
 
         """
         self.lammps_cell = self.to_cell_matrix(xyz, offdiag)
-        return np.dot(self.lammps_cell, self.R.T)
+        lc = self.lammps_cell.copy()
+
+        # reverse flip
+        if self.flip[0]:
+            lc[1][0] -= np.sign(lc[1][0])*lc[0][0]
+        if self.flip[1]:
+            lc[2][0] -= np.sign(lc[2][0])*lc[0][0]
+        if self.flip[2]:
+            lc[2][1] -= np.sign(lc[2][1])*lc[1][1]
+
+        return np.dot(lc, self.R.T)
 
     def to_cell_matrix(self, xyz, offdiag):
         """Build lammps triagonal cell from given parameters
@@ -68,7 +101,10 @@ class Prism:
         :rtype: np.array
 
         """
-        return np.dot(vec, self.R)
+        # !TODO: right eps-limit
+        # lammps might not like atoms outside the cell
+        return wrap_positions(np.dot(vec, self.R), self.lammps_cell,
+                              pbc=self.pbc, eps=1e-18)
 
     def vector_to_ase(self, vec):
         """Rotate vector from lammps coordinate system to ase one
