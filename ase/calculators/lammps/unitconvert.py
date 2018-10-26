7b0ab9ad95e681093075eaa90789ca3fdd4f1792
Morten Nagel <morten.nagel@helsinki.fi>
2018-10-26 14:50:51 +0300

    LAMMPS: add more units

--- ase/calculators/lammps/unitconvert.py
+++ ase/calculators/lammps/unitconvert.py
@@ -1,5 +1,7 @@
 """LAMMPS has the options to use several internal units (which can be different
-from hte ones used in ase).  Mapping is therefore necessary
+from hte ones used in ase).  Mapping is therefore necessary.
+
+See: https://lammps.sandia.gov/doc/units.html
  """
 
 from scipy.constants import gram
@@ -8,62 +10,167 @@ from scipy.constants import angstrom
 from scipy.constants import eV
 from scipy.constants import elementary_charge
 from scipy.constants import pico
+from scipy.constants import micro
+from scipy.constants import atto
+from scipy.constants import nano
 from scipy.constants import bar
 from scipy.constants import centi
 from scipy.constants import femto
 from scipy.constants import kilo
 from scipy.constants import calorie
 from scipy.constants import atmosphere
+from scipy.constants import erg
+from scipy.constants import dyne
 
 from ase import units
-mole = N_A
 
-unitSets = {}
+PASCAL = 1.0
+SECOND = 1.0
+METER = 1.0
+KELVIN = 1.0
+JOULE = 1.0
+NEWTON = 1.0
+COULOMB = units.C
+VOLT = 1.0
 
-# !TODO add missing units
-unitSets['ASE'] = dict(
-    mass=gram/mole,
+ATOMIC_MASS_UNIT = 1. / units.kg
+MOLE = N_A
+POISE = 0.1 * PASCAL * SECOND
+STATCOULOMB = 1. / 2997924580. * COULOMB
+STATVOLT = 299.792458 * VOLT
+
+DIM = 3.
+
+UNITSETS = {}
+
+# !TODO add missing units to 'ASE'
+# !TODO add reduce Lennards-Jones units
+# !TODO cross check which CODATA version lammps uses
+UNITSETS['ASE'] = dict(
+    mass=gram / MOLE,
     distance=angstrom,
-    time=1./units.second,
+    time=1. / units.second,
     energy=eV,
-    velocity=angstrom/(1./units.second),
-    force=eV/angstrom,
-    pressure=1./units.Pascal,
-    stress=1./units.Pascal,
+    velocity=angstrom / (1. / units.second),
+    force=eV / angstrom,
+    pressure=1. / units.Pascal,
     charge=elementary_charge
 )
 
-unitSets['metal'] = dict(
-    mass=gram/mole,
+UNITSETS['real'] = dict(
+    mass=gram / MOLE,
+    distance=angstrom,
+    time=femto,
+    energy=kilo * calorie / MOLE,
+    velocity=angstrom / femto,
+    force=kilo * calorie / (MOLE * angstrom),
+    torque=kilo * calorie / MOLE,
+    temperature=KELVIN,
+    pressure=atmosphere,
+    dynamic_viscosity=POISE,
+    charge=elementary_charge,
+    electric_field=1. / angstrom,
+    density=gram / centi**DIM
+)
+
+UNITSETS['metal'] = dict(
+    mass=gram / MOLE,
     distance=angstrom,
     time=pico,
     energy=eV,
-    velocity=angstrom/pico,
-    force=eV/angstrom,
+    velocity=angstrom / pico,
+    force=eV / angstrom,
     torque=eV,
-    temperature=1.,
+    temperature=KELVIN,
     pressure=bar,
-    stress=bar,
+    dynamic_viscosity=POISE,
     charge=elementary_charge,
-    dipole=elementary_charge*angstrom,
-    electricField=1./angstrom,
-    density=gram/centi**3.
+    dipole=elementary_charge * angstrom,
+    electric_field=1. / angstrom,
+    density=gram / centi**DIM
 )
 
-unitSets['real'] = dict(
-    mass=gram/mole,
-    distance=angstrom,
-    time=femto,
-    energy=kilo*calorie/mole,
-    velocity=angstrom/femto,
-    force=kilo*calorie/(mole*angstrom),
-    torque=kilo*calorie/mole,
-    temperature=1.,
-    pressure=atmosphere,
-    stress=atmosphere,
-    charge=elementary_charge,
-    electricField=1./angstrom,
-    density=gram/centi**3.
+UNITSETS['si'] = dict(
+    mass=kilo * gram,
+    distance=METER,
+    time=SECOND,
+    energy=JOULE,
+    velocity=METER / SECOND,
+    force=NEWTON,
+    torque=NEWTON * METER,
+    temperature=KELVIN,
+    pressure=PASCAL,
+    dynamic_viscosity=PASCAL * SECOND,
+    charge=COULOMB,
+    dipole=COULOMB * METER,
+    electric_field=VOLT / METER,
+    density=kilo * gram / METER**DIM
+)
+
+UNITSETS['cgs'] = dict(
+    mass=gram,
+    distance=centi * METER,
+    time=SECOND,
+    energy=erg,
+    velocity=centi * METER / SECOND,
+    force=dyne,
+    torque=dyne * centi * METER,
+    temperature=KELVIN,
+    pressure=dyne / (centi * METER)**2,  # or barye = 1.0e-6 bars,
+    dynamic_viscosity=POISE,
+    charge=STATCOULOMB,  # or esu (4.8032044e-10 is a proton)
+    dipole=STATCOULOMB * centi * METER,  # = 10^18 debye,
+    electric_field=STATVOLT / (centi * METER),  # or dyne / esu,,
+    density=gram / (centi * METER ** DIM)
+)
+
+UNITSETS['electron'] = dict(
+    mass=ATOMIC_MASS_UNIT,
+    distance=units.Bohr,
+    time=femto * SECOND,
+    energy=units.Hartree/units.J,
+    # velocity = Bohr / atomic time units, [1.03275e-15 SECONDs]
+    velocity=units.Bohr / (units.AUT / units.second),
+    force=units.Hartree / units.Bohr,
+    temperature=KELVIN,
+    pressure=PASCAL,
+    charge=elementary_charge,  # multiple of electron charge (1.0 is a proton)
+    dipole=units.Debye,
+    electric_field=VOLT / (centi * METER),
+)
+
+UNITSETS['micro'] = dict(
+    mass=pico * gram,
+    distance=micro * METER,
+    time=micro * SECOND,
+    energy=pico * gram * (micro * METER) ** 2 / (micro * SECOND) ** 2,
+    velocity=micro * METER / micro * SECOND,
+    force=pico * gram * micro * METER / (micro * SECOND) ** 2,
+    torque=pico * gram * (micro * METER) ** 2 / (micro * SECOND) ** 2,
+    temperature=KELVIN,
+    pressure=pico * gram / (micro * METER * (micro * SECOND) ** 2),
+    dynamic_viscosity=pico * gram / (micro * METER * micro * SECOND),
+    charge=pico * COULOMB,  # (1.6021765e-7 is a proton),
+    dipole=pico * COULOMB * micro * METER,
+    electric_field=VOLT / micro * METER,
+    density=pico * gram / (micro * METER) ** DIM,
+)
+
+UNITSETS['nano'] = dict(
+    mass=atto * gram,
+    distance=nano * METER,
+    time=nano * SECOND,
+    energy=atto * gram * (nano * METER) ** 2 / (nano * SECOND) ** 2,
+    velocity=nano * METER / (nano * SECOND),
+    force=atto * gram * nano * METER / (nano * SECOND) ** 2,
+    torque=atto * gram * (nano * METER) ** 2 / (nano * SECOND) ** 2,
+    temperature=KELVIN,
+    pressure=atto * gram / (nano * METER * (nano * SECOND) ** 2),
+    dynamic_viscosity=atto * gram / (nano * METER * nano * SECOND),
+    charge=elementary_charge,  # multiple of electron charge (1.0 is a proton)
+    dipole=elementary_charge * nano * METER,
+    electric_field=VOLT / (nano * METER),
+    density=atto * gram / (nano * METER) ** DIM,
 )
 
 
@@ -72,11 +179,11 @@ def convert(value, quantity, fromunits, tounits):
 
     :param value: converted value
     :param quantity: mass, distance, time, energy, veloctiy, force, torque,
-    temperature, pressure, stress, charge, dipole,
-    electricField or density
-    :param fromunits: ase, metal or real
-    :param tounits: ase, metal or real
+    temperature, pressure, dynamic_viscosity, charge, dipole,
+    electric_field or density
+    :param fromunits: ase, metal, real or other (see lammps docs).
+    :param tounits: ase, metal, real or other
     :returns: converted value
     :rtype:
     """
-    return unitSets[fromunits][quantity]/unitSets[tounits][quantity] * value
+    return UNITSETS[fromunits][quantity] / UNITSETS[tounits][quantity] * value
