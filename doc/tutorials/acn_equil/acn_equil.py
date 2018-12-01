582618969e97a451ccd283da831aabeb97646daa
Gianluca Levi <giale@kemi.dtu.dk>
2018-12-01 20:44:52 +0100

    DOC: Add tutorial MD equilibration of acetonitrile

---
+++ doc/tutorials/acn_equil/acn_equil.py
@@ -0,0 +1,85 @@
+from ase import Atoms
+from ase.constraints import FixLinearTriatomic
+from ase.calculators.acn import (ACN, m_me, m_c,
+                                 m_n, r_mec, r_cn)
+from ase.md import Langevin
+import ase.units as units
+from ase.io import Trajectory
+
+import numpy as np
+
+
+# Set up acetonitrile box at 25 deg C density
+pos = [[0, 0, -r_mec],
+       [0, 0, 0],
+       [0, 0, r_cn]]
+atoms = Atoms('CCN', positions=pos)
+atoms.rotate(30, 'x')
+
+# First C of each molecule needs to have the mass of a methyl group
+masses = atoms.get_masses()
+masses[::3] = m_me
+atoms.set_masses(masses)
+
+vol = ((masses.sum() / 6.022140857e23) / (0.776 / 1e24))**(1 / 3.)
+atoms.set_cell((vol, vol, vol))
+atoms.center()
+
+atoms = atoms.repeat((3, 3, 3))
+atoms.set_pbc(True)
+
+# Set constraints for rigid triatomic molecules   
+nm = 27
+atoms.constraints = FixLinearTriatomic(
+                    pairs=[(3 * i, 3 * i + 2)
+                           for i in range(nm)],
+                    centers=[j * 3 + 1 for j in range(nm)],
+                    distances=[r_mec,r_cn],
+                    masses=[m_me,m_c,m_n])
+
+tag = 'acn_27mol_300K'
+atoms.calc = ACN(rc=np.min(np.diag(atoms.cell))/2)
+
+# Create Langevin object 
+# The friction on the central atom must be zero 
+fr = np.zeros((len(atoms), 3))
+fr[::3] = 0.01
+fr[2::3] = 0.01
+md = Langevin(atoms, 1 * units.fs, 
+              temperature=300 * units.kB,
+              selectlinear=[],
+              friction=fr, 
+              logfile=tag + '.log')
+
+traj = Trajectory(tag + '.traj', 'w', atoms)
+md.attach(traj.write, interval=1)
+md.run(5000)
+
+# Repeat box and equilibrate further
+tag = 'acn_216mol_300K'
+atoms.set_constraint()
+atoms = atoms.repeat((2, 2, 2))
+nm = 216
+atoms.constraints = FixLinearTriatomic(
+                    pairs=[(3 * i, 3 * i + 2)
+                           for i in range(nm)],
+                    centers=[j * 3 + 1 for j in range(nm)],
+                    distances=[r_mec,r_cn],
+                    masses=[m_me,m_c,m_n])
+atoms.calc = ACN(rc=np.min(np.diag(atoms.cell))/2)
+
+# Create Langevin object
+# The friction on the central atom must be zero
+fr = np.zeros((len(atoms), 3))
+fr[::3] = 0.01
+fr[2::3] = 0.01
+md = Langevin(atoms, 2 * units.fs,
+              temperature=300 * units.kB,
+              selectlinear=[],
+              friction=fr,
+              logfile=tag + '.log')
+
+traj = Trajectory(tag + '.traj', 'w', atoms)
+md.attach(traj.write, interval=1)
+md.run(3000)
+
---
+++ doc/tutorials/acn_equil/acn_equil.rst
@@ -0,0 +1,44 @@
+.. _Acetonitrile MD Box Equillibration:
+
+Equilibrating an MD box of acetonitrile
+=======================================
+
+In this tutorial we see how to perform a thermal equilibration of an MD
+box of classical acetonitrile molecules using the Langevin module and 
+the implementation of an acetonitrile force field in ASE.  
+
+Since the TIPnP type water interpotentials are for rigid
+molecules, there are no intramolecular force terms, and we need to
+constrain all internal degrees of freedom. For this, we're
+using the RATTLE-type constraints of the :ref:`FixBondLengths` class to
+constrain all internal atomic distances (O-H1, O-H2, and H1-H2) for
+each molecule.
+
+The box is equillibrated with the Langevin thermostat.
+
+
+For efficiency, we first equillibrate a smaller box, and then repeat that
+once more for the final equillibration. However, the potentials are not
+parallelized, and are mainly included for testing and for use with QM/MM
+tasks, so expect to let it run for some time.
+
+.. literalinclude:: acn_equil.py
+
+.. note::
+
+  The temperature calculated by ASE is assuming all degrees of freedom
+  are available to the system. Since the constraints have removed the 3
+  vibrational modes from each water, the shown temperature will be 2/3
+  of the actual value.
+
+The procedure for the TIP4P force field is the same, with the following
+exception: the atomic sequence **must** be OHH, OHH, ... .
+
+So to perform the same task using TIP4P, you simply have to import
+that calculator instead:
+
+::
+
+    from ase.calculators.tip4p import TIP4P, rOH, angleHOH
+
+More info about the TIP4P potential: :mod:`ase.calculators.tip4p`
