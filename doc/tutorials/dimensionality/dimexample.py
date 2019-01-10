fb4ffbbeaf53e98829027a4634e937755d5124ad
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-01-10 11:13:56 +0100

    PEP8

--- doc/tutorials/dimensionality/dimexample.py
+++ doc/tutorials/dimensionality/dimexample.py
@@ -4,7 +4,7 @@ from ase.geometry import analyze_dimensionality
 
 
 atoms = ase.build.mx2(formula='MoS2', kind='2H', a=3.18, thickness=3.19)
-atoms.cell[2,2] = 7
+atoms.cell[2, 2] = 7.0
 atoms.set_pbc((1, 1, 1))
 atoms *= 3
 
