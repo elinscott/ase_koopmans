4b65bd8809b3eeec8400aa55fe6b10589ecf5efc
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-01-16 13:38:08 +0100

    Skip doctests for ulm on Python 2.7

--- ase/test/doctests.py
+++ ase/test/doctests.py
@@ -1,4 +1,5 @@
 import doctest
+import sys
 
 from ase import atoms
 from ase.collections import collection
@@ -8,8 +9,11 @@ from ase.build import tools
 from ase.io import ulm
 import ase.eos as eos
 
-modules = [xtal, spacegroup, cell, findsym, ulm, atoms, eos,
+modules = [xtal, spacegroup, cell, findsym, atoms, eos,
            geometry, tools, collection]
+
+if sys.version_info > (2, 7):
+    modules.append(ulm)
 
 for mod in modules:
     print(mod, doctest.testmod(mod, raise_on_error=True))
