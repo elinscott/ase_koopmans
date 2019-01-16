7d7383b78b81229507f805372bc616f0961831a6
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-01-16 14:04:44 +0100

    remove escaping \ from doc string

--- ase/md/velocitydistribution.py
+++ ase/md/velocitydistribution.py
@@ -115,7 +115,7 @@ def phonon_harmonics(
     masses: array of length N
         masses of the structure in amu
     temp: float
-        Temperature converted to eV (T \* units.kB)
+        Temperature converted to eV (T * units.kB)
     rng: function
         Random number generator function, e.g., np.random.rand
     quantum: bool
@@ -284,7 +284,7 @@ def PhononHarmonics(
     force_constants: ndarray of size 3N x 3N
         Force constants for the the structure represented by atoms in eV/Å²
     temp: float
-        Temperature in eV (T \* units.kB)
+        Temperature in eV (T * units.kB)
     rng: Random number generator
         RandomState or other random number generator, e.g., np.random.rand
     quantum: bool
