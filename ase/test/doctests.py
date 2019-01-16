c7eb6aebe47b39a5cb4d25c18050bf6562f0d4c6
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-01-16 12:51:26 +0100

    We always have scipy and Python 2.7

--- ase/test/doctests.py
+++ ase/test/doctests.py
@@ -1,10 +1,4 @@
 import doctest
-import sys
-
-try:
-    import scipy
-except ImportError:
-    scipy = None
 
 from ase import atoms
 from ase.collections import collection
@@ -14,13 +8,8 @@ from ase.build import tools
 from ase.io import ulm
 import ase.eos as eos
 
-modules = [xtal, spacegroup, cell, findsym, ulm, atoms, eos]
-
-if scipy:
-    modules.extend([geometry, tools])
-
-if sys.version_info >= (2, 7):
-    modules.append(collection)
+modules = [xtal, spacegroup, cell, findsym, ulm, atoms, eos,
+           geometry, tools, collection]
 
 for mod in modules:
     print(mod, doctest.testmod(mod, raise_on_error=True))
