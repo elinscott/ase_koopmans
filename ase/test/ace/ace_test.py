b7d48d191e6949128b3cc0e1d9095e8907891f57
KimHyeonsu <kim1231232@kaist.ac.kr>
2019-01-24 08:35:56 +0900

    Delete mpirun command in ase_test.py

--- ase/test/ace/ace_test.py
+++ ase/test/ace/ace_test.py
@@ -3,6 +3,6 @@ from ase.calculators.ace_cal import ACE
 
 label = "test"
 mol = Atoms('H2',[(0, 0, 0),(0, 0, 0.7)])
-ace = ACE(label=label,command = 'mpirun -np 4 /home/khs/hs_file/programs/ACE-Molecule/ace PREFIX.inp > PREFIX.log')
+ace = ACE(label=label,command = '/home/khs/hs_file/programs/ACE-Molecule/ace PREFIX.inp > PREFIX.log')
 mol.set_calculator(ace)
 mol.get_potential_energy()
