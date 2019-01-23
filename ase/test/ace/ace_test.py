b00a0870bc8dbb76a69e71ee06ef8f89034c6753
KimHyeonsu <kim1231232@kaist.ac.kr>
2019-01-24 08:21:26 +0900

    Try to revise python 2.7 test error.

--- ase/test/ace/ace_test.py
+++ ase/test/ace/ace_test.py
@@ -1,10 +1,8 @@
-#import sys
 from ase import Atoms
 from ase.calculators.ace_cal import ACE
 
 label = "test"
-#mol= read('H2.xyz')
 mol = Atoms('H2',[(0, 0, 0),(0, 0, 0.7)])
 ace = ACE(label=label,command = 'mpirun -np 4 /home/khs/hs_file/programs/ACE-Molecule/ace PREFIX.inp > PREFIX.log')
 mol.set_calculator(ace)
-print ( mol.get_potential_energy())
+mol.get_potential_energy()
