17ed2fe10c04b8c9c776c4573b4ffa8f18f46eeb
KimHyeonsu <kim1231232@kaist.ac.kr>
2019-01-24 08:02:06 +0900

    Add ace in "./doc/ase/calculators/calculators.rst" toctree list

--- ase/test/ace/ace_test.py
+++ ase/test/ace/ace_test.py
@@ -5,6 +5,6 @@ from ase.calculators.ace_cal import ACE
 label = sys.argv[1]    
 #mol= read('H2.xyz')
 mol = Atoms('H2',[(0, 0, 0),(0, 0, 0.7)])
-ace = ACE(label=label)
+ace = ACE(label=label,command = 'mpirun -np 4 /home/khs/hs_file/programs/ACE-Molecule/ace PREFIX.inp > PREFIX.log')
 mol.set_calculator(ace)
 print ( mol.get_potential_energy())
