56dbb00f6ec6ecd585b6d83f7e66ebadeb6afc2d
KimHyeonsu <kim1231232@kaist.ac.kr>
2019-01-24 08:10:29 +0900

    Delete H2.xyz and revise lable in ace_test.py

--- ase/test/ace/ace_test.py
+++ ase/test/ace/ace_test.py
@@ -1,8 +1,8 @@
-import sys
+#import sys
 from ase import Atoms
 from ase.calculators.ace_cal import ACE
 
-label = sys.argv[1]    
+label = "test"
 #mol= read('H2.xyz')
 mol = Atoms('H2',[(0, 0, 0),(0, 0, 0.7)])
 ace = ACE(label=label,command = 'mpirun -np 4 /home/khs/hs_file/programs/ACE-Molecule/ace PREFIX.inp > PREFIX.log')
