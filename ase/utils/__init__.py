b1526b6d364e07d16689364914cb540e61e03801
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-03-11 07:48:35 +0100

    Fix merge conflict again

--- ase/utils/__init__.py
+++ ase/utils/__init__.py
@@ -364,23 +364,6 @@ def hsv(array, s=.9, v=.9):
     return np.reshape(result, array.shape + (3,))
 
 
-def natural_cutoffs(atoms, mult=1, **kwargs):
-    """Generate a radial cutoff for every atom based on covalent radii.
-
-    The covalent radii are a reasonable cutoff estimation for bonds in
-    many applications such as neighborlists, so function generates an
-    atoms length list of radii based on this idea.
-
-    * atoms: An atoms object
-    * mult: A multiplier for all cutoffs, useful for coarse grained adjustment
-    * kwargs: Symbol of the atom and its corresponding cutoff, used to override
-      the covalent radii
-    """
-    from ase.data import covalent_radii
-    return [kwargs.get(atom.symbol, covalent_radii[atom.number] * mult)
-            for atom in atoms]
-
-
 # This code does the same, but requires pylab
 # def cmap(array, name='hsv'):
 #     import pylab
