2feb5dc6250017109552c054045f7048ec70a2e3
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-03-14 22:31:15 +0100

    implement jsonable decorator to auotmatically provide read() and write() functions from lightweight JSON objects (band structures etc)

--- ase/io/jsonio.py
+++ ase/io/jsonio.py
@@ -4,23 +4,9 @@ import json
 import numpy as np
 from ase.utils import reader, writer
 
+
 class MyEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, np.ndarray) or hasattr(obj, '__array__'):
-            # XXX the __array__ check will save cells as numpy arrays.
-            # In the future we should perhaps save it as a Cell, so that
-            # it can be restored as a Cell.  We should allow this with
-            # other objects as well through todict().
-            if obj.dtype == complex:
-                return {'__complex_ndarray__': (obj.real.tolist(),
-                                                obj.imag.tolist())}
-            return obj.tolist()
-        if isinstance(obj, np.integer):
-            return int(obj)
-        if isinstance(obj, np.bool_):
-            return bool(obj)
-        if isinstance(obj, datetime.datetime):
-            return {'__datetime__': obj.isoformat()}
         if hasattr(obj, 'todict'):
             d = obj.todict()
 
@@ -32,6 +18,17 @@ class MyEncoder(json.JSONEncoder):
                 d['__ase_objtype__'] = obj.ase_objtype
 
             return d
+        if isinstance(obj, np.ndarray) or hasattr(obj, '__array__'):
+            if obj.dtype == complex:
+                return {'__complex_ndarray__': (obj.real.tolist(),
+                                                obj.imag.tolist())}
+            return obj.tolist()
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.bool_):
+            return bool(obj)
+        if isinstance(obj, datetime.datetime):
+            return {'__datetime__': obj.isoformat()}
         return json.JSONEncoder.default(self, obj)
 
 
@@ -49,17 +46,22 @@ def object_hook(dct):
     if '__ase_objtype__' in dct:
         objtype = dct.pop('__ase_objtype__')
         dct = numpyfy(dct)
-        if objtype == 'bandstructure':
+
+        # We just try each object type one after another and instantiate
+        # them manually, depending on which kind it is.
+        # We can formalize this later if it ever becomes necessary.
+        if objtype == 'cell':
+            from ase.geometry.cell import Cell
+            obj = Cell(**dct)
+        elif objtype == 'bandstructure':
             from ase.dft.band_structure import BandStructure
             obj = BandStructure(**dct)
         elif objtype == 'bandpath':
             from ase.dft.kpoints import BandPath
-            from ase.geometry.cell import Cell
-            dct['cell'] = Cell(dct['cell'])
-            # XXX We will need Cell to read/write itself so it also has pbc!
             obj = BandPath(**dct)
         else:
-            raise KeyError('Cannot handle type: {}'.format(objtype))
+            raise RuntimeError('Do not know how to decode object type {} '
+                               'into an actual object'.format(objtype))
 
         assert obj.ase_objtype == objtype
         return obj
@@ -109,3 +111,44 @@ def read_json(fd):
 @writer
 def write_json(fd, obj):
     fd.write(encode(obj))
+
+
+# The next two functions are for hotplugging into a JSONable class
+# using the jsonable decorator:
+
+def write(self, fd):
+    """Write to JSON file."""
+    write_json(fd, self)
+
+
+@classmethod
+def read(cls, fd):
+    """Read new instance from JSON file."""
+    obj = read_json(fd)
+    assert type(obj) is cls
+    return obj
+
+
+def jsonable(name):
+    """Decorator for facilitating JSON I/O with a class.
+
+    Pokes JSON-based read and write functions into the class.
+
+    In order to write an object to JSON, it needs to be a known simple type
+    (such as ndarray, float, ...) or implement todict().  If the class
+    defines a string called ase_objtype, the decoder will want to convert
+    the object back into its original type when reading."""
+    def jsonableclass(cls):
+        cls.ase_objtype = name
+        if not hasattr(cls, 'todict'):
+            raise TypeError('Class must implement todict()')
+
+        # We may want the write and read to be optional.
+        # E.g. a calculator might want to be JSONable, but not
+        # that .write() produces a JSON file.
+        #
+        # This is mostly for 'lightweight' object IO.
+        cls.write = write
+        cls.read = read
+        return cls
+    return jsonableclass
