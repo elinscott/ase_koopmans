5564050878bd0574de2cef6d3a2f97fd25c85a85
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-03-14 23:02:41 +0100

    move jsonable decorator to ase/utils since it must be possible to import it at class definition time, and we probably don't want ase.io at that time (also: trouble with cyclic imports).  Make BandStructure and BandPath JSONable.

--- ase/test/bandstructure_json.py
+++ ase/test/bandstructure_json.py
@@ -1,5 +1,5 @@
 from ase.build import bulk
-from ase.dft.band_structure import calculate_band_structure
+from ase.dft.band_structure import calculate_band_structure, BandStructure
 from ase.io.jsonio import read_json
 from ase.calculators.test import FreeElectrons
 
@@ -14,8 +14,8 @@ bs.write('bs.json')
 bs.path.write('path.json')
 
 bs1 = read_json('bs.json')
+bs2 = BandStructure.read('bs.json')
 path1 = read_json('path.json')
-print(bs1)
-print(path1)
 assert type(bs1) == type(bs)
+assert type(bs2) == type(bs)
 assert type(path1) == type(bs.path)
--- ase/utils/__init__.py
+++ ase/utils/__init__.py
@@ -409,6 +409,51 @@ def reader(func):
     return iofunction(func, 'r')
 
 
+# The next two functions are for hotplugging into a JSONable class
+# using the jsonable decorator.  We are supposed to have this kind of stuff
+# in ase.io.jsonio, but we'd rather import them from a 'basic' module
+# like ase/utils than one which triggers a lot of extra (cyclic) imports.
+
+def write_json(self, fd):
+    """Write to JSON file."""
+    from ase.io.jsonio import write_json as _write_json
+    _write_json(fd, self)
+
+
+@classmethod
+def read_json(cls, fd):
+    """Read new instance from JSON file."""
+    from ase.io.jsonio import read_json as _read_json
+    obj = _read_json(fd)
+    assert type(obj) is cls
+    return obj
+
+
+def jsonable(name):
+    """Decorator for facilitating JSON I/O with a class.
+
+    Pokes JSON-based read and write functions into the class.
+
+    In order to write an object to JSON, it needs to be a known simple type
+    (such as ndarray, float, ...) or implement todict().  If the class
+    defines a string called ase_objtype, the decoder will want to convert
+    the object back into its original type when reading."""
+    def jsonableclass(cls):
+        cls.ase_objtype = name
+        if not hasattr(cls, 'todict'):
+            raise TypeError('Class must implement todict()')
+
+        # We may want the write and read to be optional.
+        # E.g. a calculator might want to be JSONable, but not
+        # that .write() produces a JSON file.
+        #
+        # This is mostly for 'lightweight' object IO.
+        cls.write = write_json
+        cls.read = read_json
+        return cls
+    return jsonableclass
+
+
 class ExperimentalFeatureWarning(Warning):
     pass
 
