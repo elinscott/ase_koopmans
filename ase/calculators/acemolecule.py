9682e5da9fe2f573df38c458e7f4b4b8ca4558eb
KimHyeonsu <kim1231232@kaist.ac.kr>
2019-03-21 21:46:41 +0900

    Now we can change parameters without error

--- ase/calculators/acemolecule.py
+++ ase/calculators/acemolecule.py
@@ -23,7 +23,6 @@ class ACE(FileIOCalculator):
                   'KineticMatrix': 'Finite_Difference', 'DerivativesOrder': '7',
                   'GeometryFilename': None, 'NumElectrons': None}
                   ]
-    guess_list = [{}]  # now not need this
     scf_list = [{
         'ExchangeCorrelation': {'XFunctional': 'GGA_X_PBE', 'CFunctional': 'GGA_C_PBE'},
         'NumberOfEigenvalues': None,
@@ -34,7 +33,10 @@ class ACE(FileIOCalculator):
         'SortOrbital': 'Order', 'MaximumOrder': '10',
         'ExchangeCorrelation': {'XFunctional': 'GGA_X_PBE', 'CFunctional': 'GGA_C_PBE'},
     }]
+    
+    
     order_list = ['BasicInformation', 'Guess', 'Scf']
+    guess_list = [{}]  # now not need this
     cis_list = [{}]
     cisd_list = [{}]
     dda_list = [{}]
@@ -53,7 +55,15 @@ class ACE(FileIOCalculator):
 
 
     def get_property(self, name, atoms=None, allow_calculation=True):
-        '''Make input, xyz after that calculate and get_property(energy, forces, and so on)'''
+        '''Make input, xyz after that calculate and get_property(energy, forces, and so on)
+           And when you want to calculate optimziation calculation and input doesn't have Force section, this fuctions automatically wrtie Force section in input.
+
+            Parameters
+            ==========
+            name:str
+                It means implimented_properties
+            atoms : ase.atoms
+        '''
         force_in_param = 0
         if(name=='forces'):
             if not 'Force' in self.parameters["order"]:
@@ -68,6 +78,12 @@ class ACE(FileIOCalculator):
         return result
 
     def set(self, **kwargs):
+        '''Change or add parameters and return changed parameter
+            Parameters
+            ==========
+            kwargs:dict
+                It is parameter that we want to change
+        '''
         new_parameters = deepcopy(self.parameters)
         changed_parameters = FileIOCalculator.set(self, **kwargs)
         if 'order' in kwargs:
@@ -80,7 +96,7 @@ class ACE(FileIOCalculator):
                     for i in range(repeat-1):
                         if value in self.default_parameters.keys():
                             new_parameters[value] += self.default_parameters[value]
-        for key in new_parameters['order']: 
+        for key in new_parameters['order']:             
             if key in kwargs.keys():      # key : BasicInformation, Force, Scf and so on
                 if isinstance(kwargs[key], dict):
                     dict_to_list = []
@@ -89,7 +105,9 @@ class ACE(FileIOCalculator):
                 i = 0
                 for val in kwargs[key]:
                     if(len(new_parameters[key])>0):
-                        new_parameters[key][i] = update_recursively(new_parameters[key][i], val)
+                        new_parameters[key][i] = update_parameter(new_parameters[key][i], val)
+                            
+                            
                     else:    
                         new_parameters[key] = [val] 
                     i = i+1
@@ -104,10 +122,12 @@ class ACE(FileIOCalculator):
         self.read_results()
 
     def make_xyz_file(self, atoms):
+        '''Writes the xyz file '''
         atoms.write("{}_opt.xyz".format(self.label))
 
     def write_input(self, atoms, properties=None, system_changes=None):
-        '''Writes the input file and xyz file'''
+        '''Writes the input file and xyz file. 
+        '''
         FileIOCalculator.write_input(self, atoms, properties, system_changes)
         inputfile = open(self.label + '.inp', 'w')
         self.make_xyz_file(atoms)
@@ -119,7 +139,9 @@ class ACE(FileIOCalculator):
         inputfile.close()
 
     def read_results(self):
-        '''Read results from logfile '''
+        '''Read results from logfile. results consist of quantites. And default quantities = [energy, forces, atoms, excitation-energy]
+            temporarily excitation-energy value is 1. This is becuase preferentially we want to calculate TDDFT by using ase.
+        '''
         filename = self.label + '.log'
         f = open(filename, "r")
         tddft = len(f.read().split("TDDFT"))
@@ -132,6 +154,7 @@ class ACE(FileIOCalculator):
                 filename, quantity=value)
 
     def write_acemolecule_section(self, fpt, section, indent=0):
+        '''Write parameters in each section of input '''
         for key, val in section.items():
             if isinstance(val, str) or isinstance(val, int) or isinstance(val, float):
                 fpt.write('    ' * indent + str(key) + " " + str(val) + "\n")
@@ -143,6 +166,57 @@ class ACE(FileIOCalculator):
         return
 
     def write_acemolecule_input(self, fpt, param2, indent=0):
+        '''Write ACE-Molecule input 
+
+            Our input format Examples
+            ========================= 
+        %% BasicInformation
+            Type	Scaling
+            Scaling	0.4
+            Basis	Sinc
+            Cell	10.0
+            Grid	Sphere
+            GeometryFormat		xyz
+            SpinMultiplicity	3.0
+            Polarize    1
+            Centered		0
+            OccupationMethod		ZeroTemp
+            %% Pseudopotential
+                Pseudopotential	1
+                UsingDoubleGrid 0
+                FilterType Sinc
+                Format	upf
+                PSFilePath	/home/khs/DATA/UPF
+                PSFileSuffix	.pbe-theos.UPF
+            %% End
+            GeometryFilename	xyz/C.xyz
+        %% End
+        %% Guess
+        %% End
+        %% Scf
+            IterateMaxCycle	150
+            ConvergenceType	Energy
+            ConvergenceTolerance	0.00001
+            EnergyDecomposition		1
+            ComputeInitialEnergy		1
+            %% Diagonalize
+                DiagonalizeMaxIter	10
+                Tolerance	0.000001
+                FullOrthogonalize		1
+            %% End
+            %% ExchangeCorrelation
+                XFunctional		GGA_X_PBE
+                CFunctional		GGA_C_PBE
+            %% End
+            %% Mixing
+                MixingMethod	1
+                MixingType	Density
+                MixingParameter	0.5
+                PulayMixingParameter	0.1
+            %% End
+        %% End
+        
+         '''
         prefix = "    " * indent
         param = deepcopy(param2)
         for i in range(len(param['order'])):
@@ -155,16 +229,19 @@ class ACE(FileIOCalculator):
         return
 
 
-def update_recursively(oldpar, newpar):
+def update_parameter(oldpar, newpar):
     for key, val in newpar.items():
-        if isinstance(val, dict):
-#            print('update_recursively if')
-            if isinstance(oldpar.get(key), dict):
-                update_recursively(old, val)
+        if key in oldpar:
+            if isinstance(val,dict):
+                update_parameter(oldpar[key],val)
             else:
-                oldpar[key] = val
+                print('newpar update')
+                print(newpar)
+                oldpar.update(newpar)
         else:
-            oldpar[key] = val
+            oldpar.update(newpar)
+            
+             
     return oldpar
 
 
