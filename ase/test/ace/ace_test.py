dc84d3e217af26e4a52b75fbf83181f83d2bccf7
KimHyeonsu <kim1231232@kaist.ac.kr>
2019-03-26 16:17:07 +0900

    Fix test/ace/ace_test.py

--- ase/test/ace/ace_test.py
+++ ase/test/ace/ace_test.py
@@ -6,4 +6,5 @@ mol = Atoms('H2',[(0, 0, 0),(0, 0, 0.7)])
 basic = [dict(Cell= '5.0')]
 ace = ACE(label=label,BasicInformation = basic, command = '/PATH/TO/EXECUTABLE/ACE-Molecule/FILE PREFIX.inp > PREFIX.log')
 mol.set_calculator(ace)
-mol.get_potential_energy()
+mol.get_forces()
+#mol.get_potential_energy()
