86c24b58396bf07d9f7ee9c88a7f3e2b2f951adb
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-04-16 19:22:22 +0200

    arduously list all the created svg files

--- doc/ase/dft/bz.py
+++ doc/ase/dft/bz.py
@@ -1,4 +1,10 @@
 # creates: bztable.rst
+# creates: 00.CUB.svg 01.FCC.svg 02.BCC.svg 03.TET.svg 04.BCT1.svg
+# creates: 05.BCT2.svg 06.ORC.svg 07.ORCF1.svg 08.ORCF2.svg 09.ORCF3.svg
+# creates: 10.ORCI.svg 11.ORCC.svg 12.HEX.svg 13.RHL1.svg 14.RHL2.svg
+# creates: 15.MCL.svg 16.MCLC1.svg 17.MCLC3.svg 18.MCLC5.svg 19.TRI1a.svg
+# creates: 20.TRI1b.svg 21.TRI2a.svg 22.TRI2b.svg
+
 import numpy as np
 import matplotlib.pyplot as plt
 
