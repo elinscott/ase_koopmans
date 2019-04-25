804d018111bf3ec3eb86d025478a4542b0a9d89b
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-04-25 09:55:15 +0200

    Skip formula doctest for old Python

--- ase/test/doctests.py
+++ ase/test/doctests.py
@@ -11,11 +11,12 @@ import ase.eos as eos
 import ase.formula as formula
 
 modules = [xtal, spacegroup, cell, findsym, atoms, eos,
-           geometry, tools, collection, formula]
+           geometry, tools, collection]
 
 if sys.version_info >= (3, 6):
     # dicts must be ordered
     modules.append(ulm)
+    modules.append(formula)
 
 for mod in modules:
     print(mod, doctest.testmod(mod, raise_on_error=True))
