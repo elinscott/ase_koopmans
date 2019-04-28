6774c14af1d0d5c4b62424cd2b0766e4517043bb
David Kleiven <davidkleiven446@gmail.com>
2019-04-28 16:25:46 +0200

    Read and write for MySQL works

--- ase/db/mysql.py
+++ ase/db/mysql.py
@@ -1,25 +1,98 @@
 from pymysql import connect
+from pymysql.err import ProgrammingError
+from copy import deepcopy
 
 from ase.db.sqlite import SQLite3Database
 from ase.db.sqlite import init_statements
-from ase.db.sqlite import index_statements
+from ase.db.sqlite import VERSION
 
 
 class Connection(object):
     def __init__(self, host=None, user=None, passwd=None,
                  db_name=None, sshtunnel=None):
         self.sshtunnel = sshtunnel
-        self.con = connect(host=self.host, user=self.user, passwd=self.passwd,
-                           db=self.db_name)
+        self.con = connect(host=host, user=user, passwd=passwd, db=db_name)
 
     def cursor(self):
-        return self.con.cursor()
+        return MySQLCursor(self.con.cursor())
 
     def commit(self):
         self.con.commit()
 
     def close(self):
         self.con.close()
+
+
+class MySQLCursor(object):
+    # MySQL has some reserved words that is not reserved
+    # in SQLite. Hence, we need to redefine those words
+    # As these words are hardcoded into the code, we cannot
+    # simply rename them during initialization
+    table_redefines = {
+        'keys': 'attribute_keys',
+    }
+
+    field_redefines = {
+        'key': 'attribute'
+    }
+
+    invalid_mysql_tables = ['number_key_values', 'keys', 'text_key_values']
+
+    def __init__(self, cur):
+        self.cur = cur
+
+    def _is_select_statement(self, sql):
+        return sql.lower().startswith('select')
+
+    def _is_update_statement(self, sql):
+        return sql.lower().startswith('update')
+
+    def _is_insert_statement(self, sql):
+        return sql.lower().startswith('insert into')
+
+    def _is_delete_statement(self, sql):
+        return sql.lower().startswith('delete')
+
+    def _is_known_statement(self, sql):
+        return self._is_select_statement(sql) or \
+            self._is_update_statement(sql) or \
+            self._is_insert_statement(sql) or \
+            self._is_delete_statement(sql)
+
+    def _redefine_invalid_tables(self, sql):
+        return sql
+        for invalid in self.invalid_mysql_tables:
+            if invalid in sql:
+                sql = sql.replace(k, v)
+        return sql
+
+    def execute(self, sql, params=None):
+        if ' keys ' in sql:
+            if not self._is_known_statement(sql):
+                raise ValueError('{} is unknown'.format(sql))
+            sql = sql.replace(' keys ', ' {} '.format(self.table_redefines['keys']))
+
+        sql = sql.replace('?', '%s')
+        if params is None:
+            params = ()
+        self.cur.execute(sql, params)
+
+    def fetchone(self):
+        return self.cur.fetchone()
+
+    def fetchall(self):
+        return self.cur.fetchall()
+
+    def executemany(self, sql, values):
+        #sql = self._redefine_invalid_tables(sql)
+        print(' keys ' in sql, self._is_insert_statement(sql), sql.lower())
+        if ' keys ' in sql:
+            if not self._is_known_statement(sql):
+                raise ValueError('{} is unknown'.format(sql))
+            sql = sql.replace(' keys ', ' {} '.format(self.table_redefines['keys']))
+        print(sql, values)
+        sql = sql.replace('?', '%s')
+        self.cur.executemany(sql, values)
 
 
 class MySQLDatabase(SQLite3Database):
@@ -47,8 +120,8 @@ class MySQLDatabase(SQLite3Database):
         self.db_name = splitted[3]
 
     def _connect(self):
-        return Connection(host=self.host, user=self.user, passwd=self.passwd,
-                          db_name=self.db_name)
+        return Connection(host=self.host, user=self.username,
+                          passwd=self.passwd, db_name=self.db_name)
 
     def _initialize(self, con):
         if self.initialized:
@@ -58,34 +131,73 @@ class MySQLDatabase(SQLite3Database):
 
         information_exists = True
         try:
-            cur.execute("SELECT 1 FROM 'information'")
-        except Exception as exc:
-            print(type(exc))
+            cur.execute("SELECT 1 FROM information")
+        except ProgrammingError as exc:
             information_exists = False
 
         if not information_exists:
             # We need to initialize the DB
-            sql = schema_update(init_statements)
-            cur.execute(sql)
+            # MySQL require that id is explicitly set as primary key
+            # in the systems table
+            init_statements[0] = init_statements[0][:-1] + ', PRIMARY KEY(id))'
+
+            statements = schema_update(deepcopy(init_statements))
+            for statement in statements:
+                cur.execute(statement)
 
             if self.create_indices:
-                cur.execute(index_statements)
+                print("Warning! The MySQL implementation does currently "
+                      "not support indexing because of the datatype TEXT "
+                      "cannot be hashed.")
             con.commit()
+            self.version = VERSION
+        else:
+            cur.execute('select * from information')
+
+            for name, value in cur.fetchall():
+                if name == 'version':
+                    self.version = int(value)
 
         self.initialized = True
 
+    def blob(self, array):
+        if array is None:
+            return None
+        return super(MySQLDatabase, self).blob(array).tobytes()
 
-def schema_update(sql):
-    for a, b in [('REAL', 'DOUBLE'),
-                 ('INTEGER PRIMARY KEY AUTOINCREMENT',
-                  'INT NOT NULL AUTO_INCREMENT')]:
-        sql = sql.replace(a, b)
+    def get_last_id(self, cur):
+        cur.execute("SELECT AUTO_INCREMENT FROM information_schema.TABLES "
+                    "WHERE TABLE_SCHEMA = '{}' AND TABLE_NAME = 'systems'"
+                    "".format(self.db_name))
+        return cur.fetchone()[0] - 1
+
+
+def schema_update(statements):
+    for i, statement in enumerate(statements):
+        for a, b in [('REAL', 'DOUBLE'),
+                     ('INTEGER PRIMARY KEY AUTOINCREMENT',
+                      'INT NOT NULL AUTO_INCREMENT')]:
+            statements[i] = statement.replace(a, b)
+
+    # MySQL does not support UNIQUE constraint on TEXT
+    # need to use VARCHAR. The unique_id is generated with
+    # randint(16**31, 16**32-1) so it will contain 32
+    # hex-characters
+    statements[0] = statements[0].replace('TEXT UNIQUE', 'VARCHAR(32) UNIQUE')
+    statements[2] = statements[2].replace('keys', 'attribute_keys')
 
     txt2jsonb = ['calculator_parameters', 'key_value_pairs', 'data']
 
     for column in txt2jsonb:
-        sql = sql.replace('{} TEXT,'.format(column),
-                          '{} JSON,'.format(column))
+        statements[0] = statements[0].replace(
+            '{} TEXT,'.format(column),
+            '{} JSON,'.format(column))
 
-    return sql
+    tab_with_key_field = ['attribute_keys', 'number_key_values',
+                          'text_key_values']
 
+    for i, statement in enumerate(statements):
+        for tab in tab_with_key_field:
+            if tab in statement:
+                statements[i] = statement.replace('key TEXT', 'attribute_key TEXT')
+    return statements
--- ase/test/db/mysql_tests.py
+++ ase/test/db/mysql_tests.py
@@ -1,4 +1,7 @@
 from ase.db import connect
+from ase import Atoms
+from ase.calculators.emt import EMT
+from ase.build import molecule
 
 HOST = 'localhost'
 USER = 'davidkl'
@@ -18,4 +21,38 @@ def test_connect():
     assert db.passwd == PASSWD
     assert db.db_name == DB_NAME
 
+
+def test_write_read():
+    db = connect(full_db_name())
+
+    co = Atoms('CO', positions=[(0, 0, 0), (0, 0, 1.1)])
+    uid = db.write(co, tag=1, type='molecule')
+
+    co_db = db.get(id=uid)
+    atoms_db = co_db.toatoms()
+
+    assert len(atoms_db) == 2
+    assert atoms_db[0].symbol == co[0].symbol
+    assert atoms_db[1].symbol == co[1].symbol
+    assert co_db.tag == 1
+    assert co_db.type == 'molecule'
+
+
+def test_write_read_with_calculator():
+    db = connect(full_db_name())
+
+    h2o = molecule('H2O')
+    calc = EMT(dummy_param=2.4)
+    h2o.set_calculator(calc)
+
+    uid = db.write(h2o)
+
+    h2o_db = db.get(id=uid).toatoms(attach_calculator=True)
+
+    calc_db = h2o_db.get_calculator()
+    assert calc_db.parameters['dummy_param'] == 2.4
+
+
 test_connect()
+test_write_read()
+test_write_read_with_calculator()
