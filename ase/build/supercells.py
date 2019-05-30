9dfdd6146bfe36bac88fc463e8afe4a10f65ffb5
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-05-30 18:29:04 +0200

    include pymatgen copyright notice for `lattice_points_in_supercell()`

--- ase/build/supercells.py
+++ ase/build/supercells.py
@@ -1,4 +1,15 @@
-"""Helper functions for creating supercells."""
+"""Helper functions for creating supercells.
+
+    The function `lattice_points_in_supercell()` is adapted from pymatgen 
+    which is available under the MIT license. This is the copyright notice:
+    
+    
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."""
 
 import numpy as np
 
@@ -191,7 +202,12 @@ def make_supercell(prim, P, wrap=True, tol=1e-5):
 
 
 def lattice_points_in_supercell(supercell_matrix):
-    """Find all lattice points contained in a supercell"""
+    """Find all lattice points contained in a supercell.
+
+    Adapted from pymatgen, which is available under MIT license:
+    The MIT License (MIT) Copyright (c) 2011-2012 MIT & The Regents of the 
+    University of California, through Lawrence Berkeley National Laboratory
+    """
 
     diagonals = np.array(
         [
