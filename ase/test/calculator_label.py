c824f8d104ecda712caf9dd0624118004a527eae
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-06-10 16:12:34 +0200

    Calculator constructor: move new 'directory' keyword to right before **kwargs so we keep compatibility with functions that pass parameters positionally.  Test label=None case in the calculator labels test

--- ase/test/calculator_label.py
+++ ase/test/calculator_label.py
@@ -3,6 +3,7 @@ from ase.calculators.calculator import Calculator
 calc = Calculator()
 assert calc.directory == '.'
 assert calc.prefix is None
+assert calc.label is None
 
 calc.label = 'dir/pref'
 assert calc.directory == 'dir'
@@ -18,3 +19,8 @@ calc.label = 'hello'
 assert calc.directory == '.'
 assert calc.prefix == 'hello'
 assert calc.label == 'hello'
+
+calc.label = None
+assert calc.label is None
+assert calc.prefix is None
+assert calc.directory == '.'
