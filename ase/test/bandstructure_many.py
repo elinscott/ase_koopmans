e9791672d04e81f9c77ff11d6f05f9dc5d14afa3
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-06-13 17:37:17 +0200

    minor adjustments to test

--- ase/test/bandstructure_many.py
+++ ase/test/bandstructure_many.py
@@ -8,22 +8,17 @@ import matplotlib.pyplot as plt
 def test():
     ax = plt.gca()
 
-    calc = FreeElectrons(nvalence=1)
     for i, lat in enumerate(all_variants()):
         if lat.ndim == 2:
             break
         xid = '{:02d}.{}'.format(i, lat.variant.name)
-        atoms = Atoms(cell=lat.tocell())
-        atoms.calc = calc
-        path = lat.bandpath(density=80)
+        path = lat.bandpath(density=60)
         path.write('path.{}.json'.format(xid))
-        calc.set(kpts=path.kpts)
-
-        atoms.calc = FreeElectrons(nvalence=1, kpts=path.kpts)
-
+        atoms = Atoms(cell=lat.tocell())
+        atoms.calc = FreeElectrons(nvalence=0, kpts=path.kpts)
         bs = calculate_band_structure(atoms, path)
         bs.write('bs.{}.json'.format(xid))
-        bs.plot(ax=ax, emin=-1, emax=22, filename='fig.{}.png'.format(xid))
+        bs.plot(ax=ax, emin=0, emax=20, filename='fig.{}.png'.format(xid))
         ax.clear()
 
 with workdir('files', mkdir=True):
