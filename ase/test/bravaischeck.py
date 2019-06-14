029475fa185afb9f7efa927f977fefa943a6bda8
Morten Gjerding <mortengjerding@gmail.com>
2019-06-14 12:50:00 +0200

    Update test to catch error

--- ase/test/bravaischeck.py
+++ ase/test/bravaischeck.py
@@ -7,21 +7,29 @@ bravais = {}
 for name in bravais1:
     bravais[name.lower()] = bravais1[name]
 
-def check_single(name, cell):
-    c = Cell(cell)
+
+def check_single(name, cell, pbc=None):
+    c = Cell(cell, pbc=pbc)
     lattice = c.get_bravais_lattice()
     name1 = lattice.name.lower()
-    ok = name.split('@')[0] == name1
+    latname = name.split('@')[0]
+    ok = latname == name1
     print(name, '-->', name1, 'OK' if ok else 'ERR', c.cellpar())
-    assert ok
+    assert ok, f'Expected {latname} but found {name1}'
+
 
 def check(name, cell):
+    if isinstance(cell, Cell):
+        pbc = cell.pbc
+    else:
+        pbc = np.array([True] * 3)
     cell = Cell(cell).array
-    #cell = Cell(cell).array
+
     # Check all three positive permutations:
-    check_single(name + '@012', cell[[0, 1, 2]])
+    check_single(name + '@012', cell[[0, 1, 2]], pbc=pbc)
     # check_single(name + '@201', cell[[2, 0, 1]])
     # check_single(name + '@120', cell[[1, 2, 0]])
+
 
 check('cub', bravais['cub'](3.3).tocell())
 check('fcc', bravais['fcc'](3.4).tocell())
@@ -43,3 +51,42 @@ check('rhl', bravais['rhl'](4., 54.).tocell())
 check('mcl', bravais['mcl'](2., 3., 4., 62.).tocell())
 check('mclc', bravais['mclc'](3., 4., 5., 70.).tocell())
 check('tri', bravais['tri'](7., 6., 5., 65., 70., 80.).tocell())
+
+# For 2D materials we have to check both the tocell() method
+# but also for realistic cell nonzero nonperiodic axis.
+check('sqr', bravais['sqr'](3.).tocell())
+check('sqr', Cell(np.diag([3., 3., 10.]),
+                  pbc=np.array([True, True, False])))
+
+check('crect', bravais['crect'](3., 40).tocell())
+
+alpha = 40 / 360 * 2 * np.pi
+a = 3
+x = np.cos(alpha)
+y = np.sin(alpha)
+
+crectcell = np.array([[a, 0, 0],
+                      [a * x, a * y, 0],
+                      [0, 0, 10]])
+check('crect', Cell(crectcell, pbc=np.array([True, True, False])))
+
+check('rect', bravais['rect'](3., 4.).tocell())
+check('rect', Cell(np.diag([3., 4., 10.]),
+                   pbc=np.array([True, True, False])))
+
+check('hex2d', bravais['hex2d'](3.).tocell())
+x = 0.5 * np.sqrt(3)
+hexcell = np.array([[a, 0, 0],
+                    [-0.5 * a, x * a, 0],
+                    [0., 0., 0.]])
+check('hex2d', Cell(hexcell, pbc=np.array([True, True, False])))
+
+check('obl', bravais['obl'](3., 4., 40).tocell())
+
+b = 4
+x = np.cos(alpha)
+y = np.sin(alpha)
+oblcell = np.array([[a, 0, 0],
+                    [b * x, b * y, 0],
+                    [0, 0, 10]])
+check('obl', Cell(oblcell, pbc=np.array([True, True, False])))
