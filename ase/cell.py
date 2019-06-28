49d86c336af31685af7c59b2710ae2e73e7fd3e3
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-06-28 18:18:58 +0200

    add docstrings to Cell

--- ase/cell.py
+++ ase/cell.py
@@ -34,6 +34,9 @@ class Cell:
         self.pbc = pbc
 
     def cellpar(self, radians=False):
+        """Get cell lengths and angles of this cell.
+
+        See also :func:`ase.geometry.cell.cell_to_cellpar`."""
         from ase.geometry.cell import cell_to_cellpar
         return cell_to_cellpar(self.array, radians)
 
@@ -46,12 +49,16 @@ class Cell:
 
     @classmethod
     def ascell(cls, cell):
+        """Return argument as a Cell object.
+
+        A new Cell object is created if necessary."""
         if isinstance(cell, cls):
             return cell
         return cls.new(cell)
 
     @classmethod
     def new(cls, cell=None, pbc=None):
+        """Create new cell from any parameters."""
         if pbc is None:
             pbc = getattr(cell, 'pbc', None)
 
@@ -78,21 +85,54 @@ class Cell:
     @classmethod
     def fromcellpar(cls, cellpar, ab_normal=(0, 0, 1), a_direction=None,
                     pbc=None):
-        """Return new Cell from cell parameters.
+        """Return new Cell from cell lengths and angles.
 
-        This is similar to cellpar_to_cell()."""
+        See also :func:`~ase.geometry.cell.cellpar_to_cell()`."""
         from ase.geometry.cell import cellpar_to_cell
         cell = cellpar_to_cell(cellpar, ab_normal, a_direction)
         return cls(cell, pbc=pbc)
 
     def get_bravais_lattice(self, eps=2e-4):
-        # We want to always reduce (so things are as robust as possible)
-        # ...or not.  It is not very reliable somehow.
+        """Return :class:`~ase.lattice.BravaisLattice` for this cell::
+
+        >>> cell = Cell.fromcellpar([4, 4, 4, 60, 60, 60])
+        >>> print(cell.get_bravais_lattice())
+        FCC(a=5.65685)
+
+        """
         from ase.lattice import get_bravais_lattice
         return get_bravais_lattice(self, eps=eps)
 
     def bandpath(self, path=None, npoints=None, density=None,
                  special_points=None, eps=2e-4):
+        """Build a :class:`~ase.dft.kpoints.BandPath` for this cell.
+
+        If special points are None, determine the Bravais lattice of
+        this cell and return a suitable Brillouin zone path with
+        standard special points.  With special points, interpolate the
+        path directly without determining Bravais lattice.
+
+        Parameters:
+
+        path: string
+            String of special point names defining the path, e.g. 'GXL'.
+        npoints: int
+            Number of points in total.  Note that at least one point
+            is added for each special point in the path.
+        density: float
+            density of kpoints along the path in Å⁻¹.
+        special_points: dict
+            Dictionary mapping special points to scaled kpoint coordinates.
+        eps: float
+            Tolerance for determining Bravais lattice.
+
+        Example
+        -------
+        >>> cell = Cell.fromcellpar([4, 4, 4, 60, 60, 60])
+        >>> cell.bandpath('GXW', npoints=20)
+        BandPath(path='GXW', special_points={GKLUWX}, kpts=[20 kpoints])
+
+        """
         # TODO: Combine with the rotation transformation from bandpath()
         if special_points is None:
             from ase.lattice import identify_lattice
@@ -118,6 +158,7 @@ class Cell:
         return Cell(complete_cell(self.array), self.pbc.copy())
 
     def copy(self):
+        """Return a copy of this cell."""
         return Cell(self.array.copy(), self.pbc.copy())
 
     @property
@@ -138,19 +179,24 @@ class Cell:
 
     @property
     def rank(self):
-        """"Dimension of the cell, i.e., number of nonzero lattice vectors."""
+        """"Return the dimension of the cell.
+
+        Equal to the number of nonzero lattice vectors."""
         # The name ndim clashes with ndarray.ndim
         return self.array.any(1).sum()
 
     @property
     def orthorhombic(self):
+        """Return whether this cell is represented by a diagonal matrix."""
         from ase.geometry.cell import is_orthorhombic
         return is_orthorhombic(self.array)
 
     def lengths(self):
+        """Return the length of each lattice vector as an array."""
         return np.array([np.linalg.norm(v) for v in self.array])
 
     def angles(self):
+        """Return an array with the three angles alpha, beta, and gamma."""
         return self.cellpar()[3:].copy()
 
     @property
@@ -176,6 +222,9 @@ class Cell:
 
     @property
     def volume(self):
+        """Get the volume of this cell.
+
+        If there are less than 3 lattice vectors, return 0."""
         # Fail or 0 for <3D cells?
         # Definitely 0 since this is currently a property.
         # I think normally it is more convenient just to get zero
@@ -185,12 +234,17 @@ class Cell:
         return self.array.tolist()
 
     def scaled_positions(self, positions):
+        """Calculate scaled positions from Cartesian positions."""
         return np.linalg.solve(self.complete().array.T, positions.T).T
 
     def cartesian_positions(self, scaled_positions):
+        """Calculate Cartesian positions from scaled positions."""
         return scaled_positions @ self.complete()
 
     def reciprocal(self):
+        """Get reciprocal lattice as a 3x3 array.
+
+        Does not include factor of 2 pi."""
         return np.linalg.pinv(self.array).transpose()
 
     def __repr__(self):
@@ -207,11 +261,20 @@ class Cell:
         return 'Cell({}, pbc={})'.format(numbers, pbc)
 
     def niggli_reduce(self, eps=1e-5):
+        """Niggli reduce this cell, returning a new cell and mapping.
+
+        See also :func:`ase.build.tools.niggli_reduce_cell`."""
         from ase.build.tools import niggli_reduce_cell
         cell, op = niggli_reduce_cell(self.array, epsfactor=1e-5)
         return Cell(cell, self.pbc), op
 
     def minkowski_reduce(self):
+        """Minkowski-reduce this cell, returning new cell and mapping.
+
+        See also :func:`ase.geometry.minkowski_reduction.minkowski_reduce`."""
         from ase.geometry.minkowski_reduction import minkowski_reduce
         rcell, op = minkowski_reduce(self)
         return Cell(rcell, self.pbc), op
+
+    # XXX We want a reduction function that brings the cell into
+    # standard form as defined by Setyawan and Curtarolo.
