0e67cffe95c187dde70e0f6ff943f8d5582ce5be
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-07-03 18:08:27 +0200

    Seem to work OK.  Cleaning up.

--- ase/db/mysql.py
+++ ase/db/mysql.py
@@ -226,13 +226,7 @@ class MySQLDatabase(SQLite3Database):
         return ase.io.jsonio.encode(remove_nan_and_inf(obj))
 
     def decode(self, obj):
-        assert isinstance(obj, str)
-        # if obj.startswith('{') and obj.endswith('}'):
-        #        obj = obj.replace('true', 'True')
-        #        obj = obj.replace('false', 'False')
-        #        obj = eval(obj)
         return insert_nan_and_inf(ase.io.jsonio.decode(obj))
-        # return insert_nan_and_inf(ase.io.jsonio.numpyfy(obj))
 
 
 def schema_update(statements):
