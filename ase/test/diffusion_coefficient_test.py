70ca5753dc1365cd69dd0f446dd0c02ad416e659
Amarjit Singh <a.singh.2313@gmail.com>
2019-08-16 16:57:02 +0000

    Added test for DiffusionCoefficient class in `ase/md/analysis.py`.

---
+++ ase/test/diffusion_coefficient_test.py
@@ -0,0 +1,24 @@
+from ase.md.analysis import DiffusionCoefficient
+from ase.atoms import Atoms
+from ase.io.trajectory import TrajectoryWriter
+
+# Creating a simple traj file
+eps = 1e-10
+a = Atoms('N3', [(0, 0, 0), (1, 0, 0), (0, 0, 1)])
+writer = TrajectoryWriter('N3.traj', mode='w')
+writer.write(a)
+a.set_positions([(2, 0, 0), (0, 2, 2), (2, 2, 0)])
+writer.write(a)
+a.set_positions([(4, 0, 0), (0, 4, 4), (4 ,4, 0)])
+writer.write(a)
+
+timestep = 1 #fs
+steps_between_images = 1
+
+diffcoeff = DiffusionCoefficient('N3.traj', timestep, steps_between_images)
+ans = diffcoeff.plot()
+
+ans_orig = 3.3333333333333335e-05
+
+assert(abs(ans - ans_orig) < eps)
+
