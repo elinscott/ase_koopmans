8c8122f1f7fed92030bba97f7196a603b5b5e163
logsdail <andrewlogsdail@hotmail.com>
2019-08-17 22:48:42 +0000

    Typo. Not sure the definition works for Trajectory as a list of Atoms, but it shows what we need.

--- ase/test/diffusion_coefficient.py
+++ ase/test/diffusion_coefficient.py
@@ -1,22 +1,26 @@
 from ase.md.analysis import DiffusionCoefficient
 from ase.atoms import Atoms
-from ase.io.trajectory import TrajectoryWriter
+#from ase.io.trajectory import TrajectoryWriter
 
 # Creating a simple traj file
 eps = 1e-10
 a = Atoms('N3', [(0, 0, 0), (1, 0, 0), (0, 0, 1)])
-writer = TrajectoryWriter('N3.traj', mode='w')
-writer.write(a)
+traj = [a.copy()]
+#writer = TrajectoryWriter('N3.traj', mode='w')
+#writer.write(a)
 a.set_positions([(2, 0, 0), (0, 2, 2), (2, 2, 0)])
-writer.write(a)
+traj.append(a.copy())
+#writer.write(a)
 a.set_positions([(4, 0, 0), (0, 4, 4), (4 ,4, 0)])
-writer.write(a)
+traj.append(a.copy())
+#writer.write(a)
 
 timestep = 1 #fs
 steps_between_images = 1
 
 # This needs to work without loading a file - it needs to accept an atoms object.
-diffcoeff = DiffusionCoefficient('N3.traj', timestep, steps_between_images)
+#diffcoeff = DiffusionCoefficient('N3.traj', timestep, steps_between_images)
+diffcoeff = DiffusionCoefficient(traj, timestep, steps_between_images)
 
 # This needs to be the functionality we have, rather than plot - we cannot have a plot in a test.
 ans = diffcoeff.calculate()
