6d863c8052e1c123babd441facaaf00cf6049a0c
logsdail <andrewlogsdail@hotmail.com>
2019-08-19 11:23:37 +0000

    Changed things around so the correct data now comes out. Still needs further tidying but the structure is looking good.

--- ase/md/analysis.py
+++ ase/md/analysis.py
@@ -1,31 +1,17 @@
-#from ase.io import read, ulm
-#from ase.io.trajectory import Trajectory
 from math import floor
 import numpy as np
 
-# Replacing with numpy.arange()
-#
-#def frange(start, stop, step=1.0):
-#    ''' "range()" like function which accept float type''' 
-#    i = start
-#    while i < stop:
-#        yield i
-#        i += step
-
 class DiffusionCoefficient:
 
-	def __init__(self, traj, timestep, steps_between_saved_images, no_of_segments=1, ignore_n_images=0, continuous=True, molecule_index=None, plot=False, data=False):
-		'''Caclulates Diffusion Coefficients and Plots the graph used for calculation
+	def __init__(self, traj, timestep, steps_between_saved_images, atom_index=None, data=False):
+		'''
+		Calculates Diffusion Coefficients for atoms and molecules
 
 		Parameters:
 			traj (Trajectory): Trajectory of atoms objects (images)
 			timestep (Int): Timestep used between each images
 			steps_between_saved_images (Int): Interval used when writing the .traj file 
-			no_of_segments (Int): Divides the given images to segments to prevent from loading all the images in the RAM at once
-			ignore_n_images (Int): Number of images you want to ignore from the start for which you don't want to calculate
-			continuous (Bool): Set True if the user wants the graph to be continuous
 			molecule_index (List of Int): The indices of atoms whose Diffusion Coefficient is to be calculated
-			plot (Bool): If the user wants to plot the graph or not
 			data (Bool): If the user wants to print the data or not
 
 			This function calculates the Diffusion Coefficient for the given .traj file using the Einstein Equation:
@@ -36,40 +22,27 @@ class DiffusionCoefficient:
 		self.traj = traj
 		self.timestep = timestep
 		self.steps_between_saved_images = steps_between_saved_images
-		self.no_of_segments = no_of_segments
-		self.ignore_n_images = ignore_n_images
-		self.continuous = continuous
-		self.molecule_index = molecule_index
-		self.plot_graph = plot
+		self.atom_index = atom_index
 		self.data = data
 
-		# Getting information for types of atoms from given file so as to calculate diffusion coefficients of different types of atoms separately
-		#traj=read(filename=self.input_file, index='0:1')
-		# We can just load the data directly as per above
-		#write(filename='temp.traj',images=temp)
-		#traj=Trajectory('temp.traj')
-		symbols=[]
-		for i in range(len(traj[0])):
-		    symbols.append(traj[0].symbols[i])
-		
-		# Moved this local to usage - ideally we remove the external package.
-		# Functionality now removed by using inbuilt "set"
-		# from collections import Counter
-
 		# Condition used if user wants to calculate Diffusion Coefficients for a specific atom or group of atoms (molecule)
-		if self.molecule_index == None:
-			#self.types_of_atoms = list(Counter(symbols).keys())
-			self.types_of_atoms = list(set(symbols))
+		if self.atom_index == None:
+			self.types_of_atoms = list(set(traj[0].get_chemical_symbols()))
 			self.no_of_types_of_atoms = len(self.types_of_atoms)
-			self.no_of_atoms = list(Counter(symbols).values())
+			self.no_of_atoms = [traj[0].get_chemical_symbols().count(symbol) for symbol in self.types_of_atoms]
 		else:
+			# This isn't strictly ideal - it only deals with one atom in a system. Better to generalise this long-term
 			self.no_of_types_of_atoms = 1
 			self.no_of_atoms = 1
-			name = ''
-			for i in self.molecule_index:
-				name += symbols[i]
-			self.types_of_atoms = [name]
-		
+			self.types_of_atoms = [traj[0].symbols[self.atom_index]]
+
+	def initialise_lists(self):
+
+		self.time_between_images = self.timestep * self.steps_between_saved_images
+		self.total_images = len(self.traj) - self.ignore_n_images
+		# List of Timestep used for plotting (x-axis)
+		self.timesteps = list(np.arange(0,self.total_images*self.time_between_images,self.time_between_images))
+
 		self.segment_ensemble_average = []
 		self.xyz_segment_ensemble_average = []
 		for i in range(self.no_of_segments):
@@ -80,18 +53,6 @@ class DiffusionCoefficient:
 				self.segment_ensemble_average[i].append([])
 				for k in range(3):
 					self.xyz_segment_ensemble_average[i][j].append([])
-		
-		self.time_between_images = self.timestep * self.steps_between_saved_images
-		
-		# Moved this local to its use, rather than global. 
-		# It'd be good to remove this external package if possible
-		#import builtins
- 		# To find out total number of images in .traj file
-		#self.total_images = ulm.read_header(builtins.open(self.input_file,'rb'))[2]
-		self.total_images = len(traj) - self.ignore_n_images
-
-		# List of Timestep used for plotting (x-axis)
-		self.timesteps = list(np.arange(0,self.total_images*self.time_between_images,self.time_between_images))
 
 		self.slopes=[]
 		self.intercepts=[]
@@ -101,80 +62,120 @@ class DiffusionCoefficient:
 
 		self.cont_xyz_segment_ensemble_average = 0
 		self.segment_line=[]
-		self.XYZ=['X','Y','Z']
-		
 
-	def calculate(self):
+	def calculate(self, ignore_n_images=0, number_of_segments=1):
+		'''
+		Calculate the diffusion coefficients.
+
+		Parameter:
+			ignore_n_images (Int): Number of images you want to ignore from the start of the trajectory, e.g. during equilibration
+			no_of_segments (Int): Divides the given trajectory in to segments to allow statistical analysis
+		'''
+
+		self.ignore_n_images = ignore_n_images
+		self.no_of_segments = number_of_segments
+
+		self.initialise_lists()
+
 		for segment_no in range(self.no_of_segments):
-			start = self.ignore_n_images + (segment_no*floor(self.total_images/self.no_of_segments))  # Making separate .traj files for each segment, so as to prevent from loading all the images in the RAM at once
+			start = self.ignore_n_images + (segment_no*floor(self.total_images/self.no_of_segments))  
 			end = start + floor(self.total_images/self.no_of_segments)
-			traj = self.traj[start:end]
-			#traj = read(filename=self.input_file,index='%d:%d'%(start,end))
-			# Not needed if we just rename the read data
-			#write(filename='temp.traj', images=temp)
-			#traj = Trajectory('temp.traj') 
-		
-			for image_no in range(1,len(traj)): ##1,len(traj)
-				#xyz_ensemble_average = [[0.0]*3]*no_of_types_of_atoms
-				xyz_ensemble_average = []
-				for i in range(self.no_of_types_of_atoms):
-					xyz_ensemble_average.append([0.,0.,0.])
+			seg = self.traj[start:end]
+
+			# For each image
+			for image_no in range(1,len(seg)): 
+				xyz_disp = [[0., 0., 0.]*self.no_of_types_of_atoms]
 				
-				
-				if self.molecule_index == None: # Calculating for all the atoms
-					for atom_no in range(len(traj[image_no])):
-						index = self.types_of_atoms.index(traj[image_no].symbols[atom_no])
-						for i in range(3):
-							xyz_ensemble_average[index][i] += np.square(traj[image_no].positions[atom_no][i] - traj[0].positions[atom_no][i])
-					for xyz in range(3):
-						for index in range(self.no_of_types_of_atoms):
-							self.xyz_segment_ensemble_average[segment_no][index][xyz].append(xyz_ensemble_average[index][xyz]/(self.no_of_atoms[index]*2))
-					for index in range(self.no_of_types_of_atoms):
-						self.segment_ensemble_average[segment_no][index].append(np.sum(xyz_ensemble_average[index])/(6*self.no_of_atoms[index]))
+				# Calculating for all the atoms
+				if self.atom_index == None: 
+					# For each atom, work out displacement from start coordinate and collect information with like atoms
+					for atom_no in range(len(seg[image_no])):
+						sym_index = self.types_of_atoms.index(seg[image_no].symbols[atom_no])
+						for xyz in range(3):
+							xyz_disp[sym_index][xyz] += np.square(seg[image_no].positions[atom_no][xyz] - seg[0].positions[atom_no][xyz])
+					
+					# For each atom species, use xyz_disp to calculate the average data
+					for sym_index in range(self.no_of_types_of_atoms):
+						# This is the average displacement for an atom species over the entire segment
+						self.segment_ensemble_average[segment_no][sym_index].append(np.sum(xyz_disp[sym_index])/(6*self.no_of_atoms[sym_index]))
+						# This is the average displacement in X, Y and Z over the entire segment
+						for xyz in range(3):
+							self.xyz_segment_ensemble_average[segment_no][sym_index][xyz].append(xyz_disp[sym_index][xyz]/(2*self.no_of_atoms[sym_index]))
+
+						
+
 		
 				else: # Calculating for specific atom or group of atoms (molecule)
 					com_orig = np.zeros(3)
 					com_new = np.zeros(3)
-					for atom_no in self.molecule_index:
+					for atom_no in self.atom_index:
 						for xyz in range(3):
-							com_orig[xyz] += traj[0].positions[atom_no][xyz]
-							com_new[xyz] += traj[image_no].positions[atom_no][xyz]
-					com_orig /= len(self.molecule_index)
-					com_new /= len(self.molecule_index)
+							com_orig[xyz] += seg[0].positions[atom_no][xyz]
+							com_new[xyz] += seg[image_no].positions[atom_no][xyz]
+					com_orig /= len(self.atom_index)
+					com_new /= len(self.atom_index)
 					for xyz in range(3):
 						xyz_ensemble_average[0][xyz] += np.square(com_new[xyz] - com_orig[xyz])
 						self.xyz_segment_ensemble_average[segment_no][0][xyz].append(xyz_ensemble_average[0][xyz]/2)
 					self.segment_ensemble_average[segment_no][0].append(np.sum(xyz_ensemble_average[0][xyz]/6))
 
 		self.cont_xyz_segment_ensemble_average = self.xyz_segment_ensemble_average
-		#os.remove('temp.traj')
 
-	def plot(self):
-		# Moved matplotlib into the function so it is not loaded unless needed
-		import matplotlib.pyplot as plt
-		
-		self.color_list = plt.cm.Set3(np.linspace(0, 1, self.no_of_types_of_atoms))
-
-		# We need to separate things out here so all the calculations are done in "calculate", and then the stored data is plotted with "plot"
-		# Calculate should return the diffusion coefficient, not plot.
-		self.calculate()
-		
 		for segment_no in range(self.no_of_segments):
-			if segment_no>0 and self.continuous==True:
+			if segment_no>0:
 			    for index in range(self.no_of_types_of_atoms):
 			        for xyz in range(3):
 			            self.cont_xyz_segment_ensemble_average[segment_no][index][xyz]=np.array(self.cont_xyz_segment_ensemble_average[segment_no][index][xyz])+self.cont_xyz_segment_ensemble_average[segment_no-1][index][xyz][-1]
 			    
 			for index in range(self.no_of_types_of_atoms):	
 				for xyz in range(3):
-					if segment_no == 0:
-						custom_label = 'Segment No. - %d ; Atom - %s ; %s'%(segment_no+1, self.types_of_atoms[index], self.XYZ[xyz])
-					else:
-						custom_label = '_Segment No. - %d ; Atom - %s ; %s'%(segment_no+1, self.types_of_atoms[index], self.XYZ[xyz]) # To remove label for other segments as all the segments are of same colour
 					ratio = floor(self.total_images/self.no_of_segments)
-					slope, intercept = 	self.fit_data_and_plot(self.timesteps[segment_no*ratio+1:segment_no*ratio+ratio], self.xyz_segment_ensemble_average[segment_no][index][xyz], plt, self.color_list[index], custom_label, self.continuous, self.cont_xyz_segment_ensemble_average[segment_no][index][xyz])
+					slope, intercept = self.fit_data(self.timesteps[segment_no*ratio+1:segment_no*ratio+ratio], self.xyz_segment_ensemble_average[segment_no][index][xyz])
 					self.slopes[index].append(slope)
 					self.intercepts[index].append(intercept)
+
+			self.segment_line.append(self.timesteps[segment_no*floor(self.total_images/self.no_of_segments)+floor(self.total_images/self.no_of_segments)-1] + self.time_between_images)
+
+		return np.mean(self.slopes)*(10**-5)
+
+	def fit_data(self,x, y):
+		# Moved local to usage
+		from scipy import stats
+
+		# Generated linear fit  
+		slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+
+		return slope, intercept
+
+	def plot(self, continuous=True):
+		'''
+		Auto-plot of Diffusion Coefficient data
+
+		Parameters:
+			continuous (Bool): Set True if the user wants the graph to be continuous
+		'''
+
+		# Moved matplotlib into the function so it is not loaded unless needed
+		import matplotlib.pyplot as plt
+		
+		color_list = plt.cm.Set3(np.linspace(0, 1, self.no_of_types_of_atoms))
+		xyz_labels=['X','Y','Z']
+
+		# We need to separate things out here so all the calculations are done in "calculate", and then the stored data is plotted with "plot"
+		# Calculate should return the diffusion coefficient, not plot.
+		if len(self.slopes) == 0:
+			diff = self.calculate()
+		
+		for segment_no in range(self.no_of_segments):
+		    
+			for index in range(self.no_of_types_of_atoms):	
+				for xyz in range(3):
+					if segment_no == 0:
+						custom_label = 'Segment No. - %d ; Atom - %s ; %s'%(segment_no+1, self.types_of_atoms[index], xyz_labels[xyz])
+					else:
+						custom_label = '_Segment No. - %d ; Atom - %s ; %s'%(segment_no+1, self.types_of_atoms[index], xyz_labels[xyz]) # To remove label for other segments as all the segments are of same colour
+					ratio = floor(self.total_images/self.no_of_segments)
+					self.plot_data(self.timesteps[segment_no*ratio+1:segment_no*ratio+ratio], self.xyz_segment_ensemble_average[segment_no][index][xyz], plt, color_list[index], custom_label, continuous, self.cont_xyz_segment_ensemble_average[segment_no][index][xyz])
 
 			self.segment_line.append(self.timesteps[segment_no*floor(self.total_images/self.no_of_segments)+floor(self.total_images/self.no_of_segments)-1] + self.time_between_images)
 
@@ -189,7 +190,7 @@ class DiffusionCoefficient:
 		# This plots the lines between the segments
 		for seg in self.segment_line:
 			plt.plot([seg,seg],[np.amin(self.cont_xyz_segment_ensemble_average), np.amax(self.cont_xyz_segment_ensemble_average)], color='grey')
-		if self.molecule_index == None:
+		if self.atom_index == None:
 			line = np.mean(self.slopes)*np.asarray(self.timesteps)+np.mean(self.intercepts)
 			diff=np.mean(self.slopes)*(10**-5)
 			plt.plot(self.timesteps, line, color='black',label='Mean')
@@ -202,33 +203,26 @@ class DiffusionCoefficient:
 		plt.xlabel('Time (fs)')
 		plt.ylabel(r'Mean Square Displacement ($\AA^2$)')
 
-		if self.plot_graph == True:
-			plt.show()
+		plt.show()
 
-		return diff
-
-	def fit_data_and_plot(self,x, y, plt, color, label=None, cont=False, cont_y=None):
+	def plot_data(self, x, y, plt, color, label=None, cont=False, cont_y=None):
 		# Moved local to usage
 		from scipy import stats
 
 		# Moved this local to its use.
 		xyz_markers = {'X':'o', 'Y':'s','Z':'^'}
 
-		# Generated linear fit  
-		slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
 		#line = slope*np.asarray(x)+intercept
 		if label[0] != '_':
 			skip = label.index('A')
 		else:
 			skip = 0
+
 		mark = xyz_markers[label[-1]]
 
 		if cont == False:
 			plt.plot(x, y, color=color, marker=mark,label=label[skip:], linewidth=0)
-			intercept_c=intercept
 		else:
-			slope, intercept_c, r_value, p_value, std_err = stats.linregress(x,cont_y)
-			#line_c = slope*np.asarray(x)+intercept_c
 			plt.plot(x, cont_y, color=color, marker=mark,label=label[skip:], linewidth=0)
 
 		if label[0] == '_':
@@ -241,4 +235,3 @@ class DiffusionCoefficient:
 		if self.data == True:
 			print('---')
 			print(r'%10s: Intercept = %.10f cm^2; Diffusion Coefficient = %.10f cm^2/s, %.10f m^2/s' % (label, intercept_c/(10**8), slope*(0.1), slope*(10**-5)))
-		return slope, intercept_c
