76b2f21bd5b6c633bb070025a95661b864773263
logsdail <andrewlogsdail@hotmail.com>
2019-08-20 22:39:00 +0000

    Tidied the test up a bit more.

--- ase/test/diffusion_coefficient.py
+++ ase/test/diffusion_coefficient.py
@@ -31,22 +31,18 @@ traj_co = [co.copy() for i in range(2)]
 traj_co[1].set_positions([(-1, -1, -1), (1, 1, 2)])
 
 dc_co = DiffusionCoefficient(traj_co, timestep, steps_between_images, molecule=False)
-dc_co.calculate(ignore_n_images=0, number_of_segments=1)
+dc_co.calculate()
 ans = dc_co.get_diffusion_coefficients()[0]
 assert(abs(ans - ans_orig) < eps)
 
-dc_co = DiffusionCoefficient(traj_co, timestep, steps_between_images, atom_indices=[0], molecule=False)
-dc_co.calculate(ignore_n_images=0, number_of_segments=1)
-ans = dc_co.get_diffusion_coefficients()[0]
-assert(abs(ans - ans_orig) < eps)
-
-dc_co = DiffusionCoefficient(traj_co, timestep, steps_between_images, atom_indices=[1], molecule=False)
-dc_co.calculate(ignore_n_images=0, number_of_segments=1)
-ans = dc_co.get_diffusion_coefficients()[0]
-assert(abs(ans - ans_orig) < eps)
+for index in range(2):
+    dc_co = DiffusionCoefficient(traj_co, timestep, steps_between_images, atom_indices=[index], molecule=False)
+    dc_co.calculate()
+    ans = dc_co.get_diffusion_coefficients()[0]
+    assert(abs(ans - ans_orig) < eps)
 
 dc_co = DiffusionCoefficient(traj_co, timestep, steps_between_images, molecule=True)
-dc_co.calculate(ignore_n_images=0, number_of_segments=1)
+dc_co.calculate()
 ans = dc_co.get_diffusion_coefficients()[0]
 ans_orig = 0.0
 assert(abs(ans - ans_orig) < eps)
