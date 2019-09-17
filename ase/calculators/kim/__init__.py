923d1c0a6def2fde5a23ba149476da4751448413
dskarls <dskpersistent@gmail.com>
2019-09-17 16:49:03 -0700

    Add get_model_supported_species to import namespace

--- ase/calculators/kim/__init__.py
+++ ase/calculators/kim/__init__.py
@@ -1,7 +1,8 @@
 import kimpy
 from .kim import KIM
+from .kim import get_model_supported_species
 
 # Ensure minimal version requirement of kimpy is satsified
 assert [int(x) for x in kimpy.__version__.split(".")] >= [0, 3, 2]
 
-__all__ = ["KIM"]
+__all__ = ["KIM", "get_model_supported_species"]
