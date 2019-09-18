431bb2b3beb6dd00f3afd01bc9a0f6b17ad4e00d
Ralf Meyer <ralf.meyer@tugraz.at>
2019-09-18 12:54:06 +0200

    TST: Increase tolerance on energy and gradient check

--- ase/test/qchem/qchem_calculator.py
+++ ase/test/qchem/qchem_calculator.py
@@ -10,7 +10,8 @@ calc = QChem(label='calc/ethane',
 
 mol.set_calculator(calc)
 # Check energy and forces
-np.testing.assert_allclose(mol.get_potential_energy(), -2172.379183703419)
+np.testing.assert_allclose(mol.get_potential_energy(), -2172.379183703419,
+    atol=10.)
 np.testing.assert_allclose(
     mol.get_forces(),
     np.array([[0., 0.00240141, 0.04992568],
@@ -21,7 +22,7 @@ np.testing.assert_allclose(
               [-0., -0.11626015, -0.07267481],
               [-0.10132204, 0.05804009, -0.07538475],
               [0.10132204, 0.05804009, -0.07538475]]),
-    atol=1e-7)
+    atol=0.05)
 
 opt = LBFGS(mol)
 opt.run()
