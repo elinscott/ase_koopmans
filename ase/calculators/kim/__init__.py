78e2a84c69ab30ecc56a86080e7f1ffbe7fa20dc
dskarls <dskpersistent@gmail.com>
2019-09-21 12:55:09 -0700

    Create requirements.txt for ase.calculators.kim

--- ase/calculators/kim/__init__.py
+++ ase/calculators/kim/__init__.py
@@ -2,7 +2,4 @@ import kimpy
 from .kim import KIM
 from .kim import get_model_supported_species
 
-# Ensure minimal version requirement of kimpy is satsified
-assert [int(x) for x in kimpy.__version__.split(".")] >= [0, 3, 2]
-
 __all__ = ["KIM", "get_model_supported_species"]
