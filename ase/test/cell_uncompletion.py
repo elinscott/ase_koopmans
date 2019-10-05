622184d968ad811f50395eac898206a5c6c2e065
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-10-05 03:31:53 +0200

    use different indexing for older python

--- ase/test/cell_uncompletion.py
+++ ase/test/cell_uncompletion.py
@@ -12,7 +12,7 @@ def test(cell):
         ucell = cell.uncomplete(pbc)
         assert ucell.rank == sum(pbc & cell.any(1))
 
-    assert all(cell.uncomplete(True).any(1) == cell.any(1))
+    assert all(cell.uncomplete(True).any(1) == cell.any(1)), (cell.uncomplete(True), cell)
     assert all(cell.uncomplete(1).any(1) == cell.any(1))
     assert cell.uncomplete(False).rank == 0
     assert cell.uncomplete(0).rank == 0
