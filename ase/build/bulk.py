1ca22f7918d5c04f10b979d88935117f0f8bf0d5
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-10-07 01:11:43 +0200

    set atomic basis correctly for RHL systems in ase.build.bulk()

--- ase/build/bulk.py
+++ ase/build/bulk.py
@@ -13,7 +13,8 @@ def incompatible_cell(*, want, have):
 
 
 def bulk(name, crystalstructure=None, a=None, b=None, c=None, *, alpha=None,
-         covera=None, u=None, orthorhombic=False, cubic=False):
+         covera=None, u=None, orthorhombic=False, cubic=False,
+         basis=None):
     """Creating bulk systems.
 
     Crystal structure and lattice constant(s) will be guessed if not
@@ -45,6 +46,7 @@ def bulk(name, crystalstructure=None, a=None, b=None, c=None, *, alpha=None,
     """
 
     if c is None and b is not None:
+        # If user passes (a, b) positionally, we want it as (a, c) instead:
         c, b = b, c
 
     if covera is not None and c is not None:
@@ -69,7 +71,7 @@ def bulk(name, crystalstructure=None, a=None, b=None, c=None, *, alpha=None,
             # have been wrong somehow.  --askhl
             raise RuntimeError('Only simple cubic ("sc") supported')
 
-    # Mapping of name to number of atoms in primitive cell
+    # Mapping of name to number of atoms in primitive cell.
     structures = {'sc': 1, 'fcc': 1, 'bcc': 1,
                   'tetragonal': 1,
                   'hcp': 1,
@@ -193,9 +195,7 @@ def bulk(name, crystalstructure=None, a=None, b=None, c=None, *, alpha=None,
     elif crystalstructure == 'tetragonal':
         atoms = Atoms(name, cell=[a, a, c], pbc=True)
     elif crystalstructure == 'rhombohedral':
-        from ase.lattice import RHL
-        lat = RHL(a, alpha)
-        atoms = Atoms(name, cell=lat.tocell(), pbc=True)
+        atoms = _build_rhl(name, a, alpha, basis)
     elif crystalstructure == 'orthorhombic':
         atoms = Atoms(name, cell=[a, b, c], pbc=True)
     else:
@@ -206,6 +206,18 @@ def bulk(name, crystalstructure=None, a=None, b=None, c=None, *, alpha=None,
     if cubic:
         assert abs(atoms.cell.angles() - 90).all() < 1e-10
     return atoms
+
+
+def _build_rhl(name, a, alpha, basis):
+    from ase.lattice import RHL
+    lat = RHL(a, alpha)
+    cell = lat.tocell()
+    if basis is None:
+        # RHL: Given by A&M as scaled coordinates "x" of cell.sum(0):
+        basis_x = reference_states[atomic_numbers[name]]['basis_x']
+        basis = basis_x[:, None].repeat(3, axis=1)
+    natoms = len(basis)
+    return Atoms([name] * natoms, cell=cell, scaled_positions=basis, pbc=True)
 
 
 def _orthorhombic_bulk(name, crystalstructure, a, covera=None, u=None):
