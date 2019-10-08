b9695fc6dd335f927c35fbf8cef6bb93b40c58ec
Peter Larsen <peter.mahler.larsen@gmail.com>
2019-10-08 17:47:10 +0000

    ENH: minkowski-reduced neighborlist

--- ase/neighborlist.py
+++ ase/neighborlist.py
@@ -1,11 +1,11 @@
-from math import sqrt
-
 import numpy as np
+import itertools
 from scipy import sparse as sp
 
 from ase.data import atomic_numbers, covalent_radii
-from ase.geometry import complete_cell
-from ase.geometry import find_mic
+from ase.geometry import complete_cell, find_mic, wrap_positions
+from ase.geometry import minkowski_reduce
+from ase.cell import Cell
 
 
 def natural_cutoffs(atoms, mult=1, **kwargs):
@@ -879,7 +879,7 @@ class PrimitiveNeighborList:
         to self.use_scaled_positions.
         """
         self.pbc = pbc = np.array(pbc, copy=True)
-        self.cell = cell = np.array(cell, copy=True)
+        self.cell = cell = Cell(cell)
         self.coordinates = coordinates = np.array(coordinates, copy=True)
 
         if len(self.cutoffs) != len(coordinates):
@@ -891,61 +891,58 @@ class PrimitiveNeighborList:
         else:
             rcmax = 0.0
 
+        if self.use_scaled_positions:
+            positions0 = cell.cartesian_positions(coordinates)
+        else:
+            positions0 = coordinates
+
+        rcell, op = minkowski_reduce(cell, pbc)
+        positions = wrap_positions(positions0, rcell, pbc=pbc, eps=0)
+
+        natoms = len(positions)
+        indices = np.arange(natoms)
+        self.nneighbors = 0
+        self.npbcneighbors = 0
+        self.neighbors = [np.empty(0, int) for a in range(natoms)]
+        self.displacements = [np.empty((0, 3), int) for a in range(natoms)]
         icell = np.linalg.pinv(cell)
 
-        if self.use_scaled_positions:
-            scaled = coordinates
-            positions = np.dot(scaled, cell)
-        else:
-            positions = coordinates
-            scaled = np.dot(positions, icell)
-
-        scaled0 = scaled.copy()
-
         N = []
+        ircell = np.linalg.pinv(rcell)
         for i in range(3):
             if self.pbc[i]:
-                scaled0[:, i] %= 1.0
-                v = icell[:, i]
-                h = 1 / sqrt(np.dot(v, v))
+                v = ircell[:, i]
+                h = 1 / np.linalg.norm(v)
                 n = int(2 * rcmax / h) + 1
             else:
                 n = 0
             N.append(n)
 
-        offsets = (scaled0 - scaled).round().astype(int)
-        positions0 = positions + np.dot(offsets, self.cell)
-        natoms = len(positions)
-        indices = np.arange(natoms)
+        for n1, n2, n3 in itertools.product(range(0, N[0] + 1),
+                                            range(-N[1], N[1] + 1),
+                                            range(-N[2], N[2] + 1)):
+            if n1 == 0 and (n2 < 0 or n2 == 0 and n3 < 0):
+                continue
 
-        self.nneighbors = 0
-        self.npbcneighbors = 0
-        self.neighbors = [np.empty(0, int) for a in range(natoms)]
-        self.displacements = [np.empty((0, 3), int) for a in range(natoms)]
-        for n1 in range(0, N[0] + 1):
-            for n2 in range(-N[1], N[1] + 1):
-                for n3 in range(-N[2], N[2] + 1):
-                    if n1 == 0 and (n2 < 0 or n2 == 0 and n3 < 0):
-                        continue
-                    displacement = np.dot((n1, n2, n3), self.cell)
-                    for a in range(natoms):
-                        d = positions0 + displacement - positions0[a]
-                        i = indices[(d**2).sum(1) <
-                                    (self.cutoffs + self.cutoffs[a])**2]
-                        if n1 == 0 and n2 == 0 and n3 == 0:
-                            if self.self_interaction:
-                                i = i[i >= a]
-                            else:
-                                i = i[i > a]
-                        self.nneighbors += len(i)
-                        self.neighbors[a] = np.concatenate(
-                            (self.neighbors[a], i))
-                        disp = np.empty((len(i), 3), int)
-                        disp[:] = (n1, n2, n3)
-                        disp += offsets[i] - offsets[a]
-                        self.npbcneighbors += disp.any(1).sum()
-                        self.displacements[a] = np.concatenate(
-                            (self.displacements[a], disp))
+            displacement = (n1, n2, n3) @ rcell
+            for a in range(natoms):
+                delta = (positions + displacement) - positions[a]
+                i = indices[(delta**2).sum(1) <
+                            (self.cutoffs + self.cutoffs[a])**2]
+                if n1 == 0 and n2 == 0 and n3 == 0:
+                    if self.self_interaction:
+                        i = i[i >= a]
+                    else:
+                        i = i[i > a]
+
+                self.nneighbors += len(i)
+                self.neighbors[a] = np.concatenate((self.neighbors[a], i))
+                v = (delta[i] - positions0[i] + positions0[a]) @ icell
+                disp = v.round().astype(np.int)
+
+                self.npbcneighbors += disp.any(1).sum()
+                self.displacements[a] = np.concatenate((self.displacements[a],
+                                                        disp))
 
         if self.bothways:
             neighbors2 = [[] for a in range(natoms)]
@@ -956,8 +953,7 @@ class PrimitiveNeighborList:
                     displacements2[b].append(-disp)
             for a in range(natoms):
                 nbs = np.concatenate((self.neighbors[a], neighbors2[a]))
-                disp = np.array(list(self.displacements[a]) +
-                                displacements2[a])
+                disp = np.array(list(self.displacements[a]) + displacements2[a])
                 # Force correct type and shape for case of no neighbors:
                 self.neighbors[a] = nbs.astype(int)
                 self.displacements[a] = disp.astype(int).reshape((-1, 3))
@@ -969,10 +965,8 @@ class PrimitiveNeighborList:
                     j = i[mask]
                     offsets = self.displacements[a][mask]
                     for b, offset in zip(j, offsets):
-                        self.neighbors[b] = np.concatenate(
-                            (self.neighbors[b], [a]))
-                        self.displacements[b] = np.concatenate(
-                            (self.displacements[b], [-offset]))
+                        self.neighbors[b] = np.concatenate((self.neighbors[b], [a]))
+                        self.displacements[b] = np.concatenate((self.displacements[b], [-offset]))
                     mask = np.logical_not(mask)
                     self.neighbors[a] = self.neighbors[a][mask]
                     self.displacements[a] = self.displacements[a][mask]
@@ -988,7 +982,7 @@ class PrimitiveNeighborList:
 
           indices, offsets = nl.get_neighbors(42)
           for i, offset in zip(indices, offsets):
-              print(atoms.positions[i] + dot(offset, atoms.get_cell()))
+              print(atoms.positions[i] + offset @ atoms.get_cell())
 
         Notice that if get_neighbors(a) gives atom b as a neighbor,
         then get_neighbors(b) will not return a as a neighbor - unless
--- ase/test/minimum_image_convention.py
+++ ase/test/minimum_image_convention.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy.testing import assert_allclose
 from ase.lattice.cubic import FaceCenteredCubic
 from ase.neighborlist import mic as NeighborListMic
+from ase.neighborlist import NeighborList, PrimitiveNeighborList
 
 
 size = 2
@@ -25,3 +26,17 @@ assert_allclose(d0, d1)
 vnbrlist = NeighborListMic(atoms.get_positions(), atoms.cell, atoms.pbc)
 d2 = np.linalg.norm(vnbrlist, axis=1)
 assert_allclose(d0, d2)
+
+
+nl = NeighborList(np.ones(len(atoms)) * 2 * size * np.sqrt(3),
+                  bothways=True,
+                  primitive=PrimitiveNeighborList)
+nl.update(atoms)
+indices, offsets = nl.get_neighbors(0)
+
+d3 = float("inf") * np.ones(len(atoms))
+for i, offset in zip(indices, offsets):
+	p = atoms.positions[i] + offset @ atoms.get_cell()
+	d = np.linalg.norm(p - atoms.positions[0])
+	d3[i] = min(d3[i], d)
+assert_allclose(d0, d3)
