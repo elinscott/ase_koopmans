36e430e77e2955dd2dd3b392ad6aab10bacccbac
dskarls <dskpersistent@gmail.com>
2019-10-13 18:30:16 -0700

    Get rid of positions in relaxation test

--- ase/test/kim/test_relax.py
+++ ase/test/kim/test_relax.py
@@ -5,24 +5,6 @@ from ase.calculators.kim import KIM
 from ase.optimize import BFGS
 
 energy_ref = -0.5420939378624228
-positions_ref = np.array(
-    [
-        [1.80450287, 1.80450287, 1.80450287],
-        [4.79991686, 1.80450287, -0.0467648],
-        [4.79991686, 1.80450287, 3.65577053],
-        [-1.19091113, 1.80450287, -0.0467648],
-        [-1.19091113, 1.80450287, 3.65577053],
-        [-0.0467648, 4.79991686, 1.80450287],
-        [3.65577053, 4.79991686, 1.80450287],
-        [-0.0467648, -1.19091113, 1.80450287],
-        [3.65577053, -1.19091113, 1.80450287],
-        [1.80450287, -0.0467648, 4.79991686],
-        [1.80450287, 3.65577053, 4.79991686],
-        [1.80450287, -0.0467648, -1.19091113],
-        [1.80450287, 3.65577053, -1.19091113],
-    ]
-)
-
 
 # Create structure
 atoms = Icosahedron("Ar", latticeconstant=3.0, noshells=2)
@@ -38,4 +20,3 @@ opt = BFGS(atoms, logfile=None)
 opt.run(fmax=0.05)
 
 assert np.isclose(atoms.get_potential_energy(), energy_ref)
-assert np.allclose(atoms.get_positions(), positions_ref)
