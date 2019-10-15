00e39e0944d972cce40d1ffa5ac52f077a11a711
dskarls <dskpersistent@gmail.com>
2019-10-15 10:15:36 -0700

    Comments/var names in test_cutoff_skin

--- ase/test/kim/test_cutoff_skin.py
+++ ase/test/kim/test_cutoff_skin.py
@@ -1,30 +1,33 @@
 """
-To test that the calculator handles skin and cutoffs correctly.
-Skin should be added to both the influence distance and cutoff.
-The cutoff for the model ex_model_Ar_P_Morse_07C is 8.15
+To test that the calculator handles skin and cutoffs correctly.  Skin
+should be added to both the influence distance and cutoff.  The cutoff
+for the model ex_model_Ar_P_Morse_07C is 8.15 Angstroms and the default
+skin distance when using the kimpy neighbor list library (which is the
+default) is 0.2 times the cutoff distance (.63 Angstroms for this
+model).
 """
 import numpy as np
 from ase.calculators.kim import KIM
 from ase import Atoms
 
 
-# create calculator
+# Create calculator
 modelname = "ex_model_Ar_P_Morse_07C"
 calc = KIM(modelname)
 
-# create an FCC crystal
-argon = Atoms("Ar2", positions=[[0, 0, 0], [0, 0, 8.2]])
+# Create an FCC argon crystal
+atoms = Atoms("Ar2", positions=[[0, 0, 0], [0, 0, 8.2]])
 
-# attach calculator to the atoms
-argon.set_calculator(calc)
+# Attach calculator to the atoms
+atoms.set_calculator(calc)
 
-# get energy and forces
-e0 = argon.get_potential_energy()
+# Get energy and forces
+e_ideal = atoms.get_potential_energy()
 
-# perturb the x coord of the first atom
-argon.positions[0, 2] += 0.1
+# Perturb the x coordinate of the first atom by less than the skin distance
+atoms.positions[0, 2] += 0.1
 
-# get energy and forces
-e1 = argon.get_potential_energy()
+# Get new energy
+e_perturb = atoms.get_potential_energy()
 
-assert not np.isclose(e0, e1)
+assert not np.isclose(e_ideal, e_perturb)
