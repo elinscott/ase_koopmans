elinscott/ase_koopmans

commit ab0f1399c7f2790770df0d217f53bd009c53dc82

ENH: Provide wrapping option for LAMMPSlib calculator
This option allows one to disable the wrapping of the positions in the
Atoms object along periodic directions, which is sometimes useful for
tracking the path taken by individual atoms during minimization or
dynamics.  If the position of an atom in the Atoms object is more than
one box length outside of the cell along a periodic dimension, it will
be lost (but LAMMPS will error out in this case).

See also https://gitlab.com/ase/ase/issues/461

ase/calculators/lammpslib.py
@@ -121,6 +121,15 @@ Keyword                                  Description
 
                       ["mass 1 58.6934"]
 
+``wrap``              Whether to wrap the atoms in the ASE object during a call to
+                      calculate() before running LAMMPS.  Note that even if this is set
+                      to False, LAMMPS will still perform its own wrapping along
+                      periodic directions. (Default: True)
+
+                      WARNING: If wrapping is disabled and atoms are more than
+                      a box length outside of the cell along a periodic
+                      dimension, they may be lost by LAMMPS.
+
 ``keep_alive``        Boolean
                       whether to keep the lammps routine alive for more commands
 
@@ -247,6 +256,7 @@ xz and yz are the tilt of the lattice vectors, all to be edited.
         atom_type_masses=None,
         log_file=None,
         lammps_name='',
+        wrap=True,
         keep_alive=False,
         lammps_header=['units metal',
                        'atom_style atomic',
@@ -310,8 +320,8 @@ xz and yz are the tilt of the lattice vectors, all to be edited.
 
         self.lmp.command(cell_cmd)
 
-    def set_lammps_pos(self, atoms, wrap=True):
-        if wrap:
+    def set_lammps_pos(self, atoms):
+        if self.parameters.wrap:
             atoms.wrap()
 
         pos = convert(atoms.get_positions(), "distance", "ASE", self.units)

commit 462b580a9286dfec8f357f08c1a6a8bc758c6673

API: Remove lammpslib in-place atom wrapping
Reverting ab0f1399c7 in favor of removing in-place wrapping of the Atoms
object positions altogether.  Not only does in-place wrapping interfere
with optimizers, but it may generally confuse users and it breaks the
encapsulation of the calculator.

Since ab0f1399c7 (merged to master ~1 week ago) introduced the `wrap`
option, this change does break backward compatibility but I think it's
probably ok.

ase/calculators/lammpslib.py
@@ -13,6 +13,7 @@ from ase.data import (atomic_numbers as ase_atomic_numbers,
                       atomic_masses as ase_atomic_masses)
 from ase.utils import basestring
 from ase.calculators.lammps import convert
+from ase.geometry import wrap_positions
 
 # TODO
 # 1. should we make a new lammps object each time ?
@@ -120,15 +121,6 @@ Keyword                                  Description
                       post initialization. (Use: Initialization amendments) e.g.
 
                       ["mass 1 58.6934"]
-
-``wrap``              Whether to wrap the atoms in the ASE object during a call to
-                      calculate() before running LAMMPS.  Note that even if this is set
-                      to False, LAMMPS will still perform its own wrapping along
-                      periodic directions. (Default: True)
-
-                      WARNING: If wrapping is disabled and atoms are more than
-                      a box length outside of the cell along a periodic
-                      dimension, they may be lost by LAMMPS.
 
 ``keep_alive``        Boolean
                       whether to keep the lammps routine alive for more commands
@@ -256,7 +248,6 @@ xz and yz are the tilt of the lattice vectors, all to be edited.
         atom_type_masses=None,
         log_file=None,
         lammps_name='',
-        wrap=True,
         keep_alive=False,
         lammps_header=['units metal',
                        'atom_style atomic',
@@ -321,10 +312,11 @@ xz and yz are the tilt of the lattice vectors, all to be edited.
         self.lmp.command(cell_cmd)
 
     def set_lammps_pos(self, atoms):
-        if self.parameters.wrap:
-            atoms.wrap()
-
-        pos = convert(atoms.get_positions(), "distance", "ASE", self.units)
+        # Create local copy of positions that are wrapped along any periodic
+        # directions
+        pos = wrap_positions(atoms.get_positions(), atoms.get_cell(),
+                atoms.get_pbc())
+        pos = convert(pos, "distance", "ASE", self.units)
 
         # If necessary, transform the positions to new coordinate system
         if self.coord_transform is not None: