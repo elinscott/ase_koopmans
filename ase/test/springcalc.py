733f2fefb1f1839e15c11af04a47dcdb3c1be3d6
Erik Fransson <karl.erik.fransson@gmail.com>
2019-11-27 21:14:00 +0100

    flake8

--- ase/test/springcalc.py
+++ ase/test/springcalc.py
@@ -1,7 +1,7 @@
 import numpy as np
 from ase.build import bulk
 from ase.calculators.harmonic import SpringCalculator
-from ase.calculators.test import numeric_force, gradient_test
+from ase.calculators.test import gradient_test
 
 
 # setup
