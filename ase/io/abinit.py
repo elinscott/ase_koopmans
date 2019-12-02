fe7588650f298b8c9356b166c97e7a8caf29a17c
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-12-02 17:45:36 +0100

    remove old write_abinit() function which was obviously duplicating the calculator's write() code.  The new one should now be able to fulfill that role.

--- ase/io/abinit.py
+++ ase/io/abinit.py
@@ -155,7 +155,7 @@ keys_with_units = {
     'latticeconstant': 'Ang'}
 
 
-def write_abinit_in(fd, atoms, param=None, species=None):
+def write_abinit(fd, atoms, param=None, species=None):
     from ase.calculators.calculator import kpts2mp
     if param is None:
         param = {}
@@ -265,103 +265,6 @@ def write_abinit_in(fd, atoms, param=None, species=None):
         fd.write('%.1f %.1f %.1f\n' % tuple((np.array(mp) + 1) % 2 * 0.5))
 
     fd.write('chkexit 1 # abinit.exit file in the running directory terminates after the current SCF\n')
-
-
-def write_abinit(filename, atoms, cartesian=False, long_format=True):
-    """Method to write abinit input files."""
-
-    import numpy as np
-    from ase import data
-
-    if isinstance(filename, basestring):
-        f = open(filename, 'w')
-    else: # Assume it's a 'file-like object'
-        f = filename
-
-    if isinstance(atoms, (list, tuple)):
-        if len(atoms) > 1:
-            raise RuntimeError("Don't know how to save more than "+
-                            "one image to input")
-        else:
-            atoms = atoms[0]
-
-    # Write atom positions in scaled or cartesian coordinates
-    if cartesian:
-        coord = atoms.get_positions()
-    else:
-        coord = atoms.get_scaled_positions()
-
-    # let us order the atoms according to chemical symbol
-    ind = np.argsort(atoms.get_chemical_symbols())
-    symbols = np.array(atoms.get_chemical_symbols())[ind]
-    coord = coord[ind]
-
-    # and now we count how many atoms of which type we have
-    sc = []
-    psym = symbols[0]
-    count = 0
-    for sym in symbols:
-        if sym != psym:
-            sc.append((psym, count))
-            psym = sym
-            count = 1
-        else:
-            count += 1
-    sc.append((psym, count))
-
-    f.write('\n# Definition of the atom types\n')
-    f.write("ntypat  " + str(len(sc)) + "\n")
-    f.write("znucl  ")
-    for specie in sc:
-        f.write(str(data.atomic_numbers[specie[0]]) + " ")
-    f.write('\n')
-
-    f.write('\n# Definition of the atoms\n')
-    f.write('natom  ' + str(len(symbols)) + '\n')
-    f.write('typat  ')
-    typat = 1
-    for specie in sc:
-        for natom in range(specie[1]):
-            f.write(str(typat) + ' ')
-        typat = typat + 1
-    f.write('\n')
-
-    f.write('\n# Definition of the unit cell\n')
-    f.write('acell\n')
-    f.write('%.14f %.14f %.14f Angstrom\n' %  (1.0, 1.0, 1.0))
-    f.write('\n')
-    f.write('rprim\n')
-    if long_format:
-        latt_form = ' %21.16f'
-    else:
-        latt_form = ' %11.6f'
-
-    for vec in atoms.get_cell():
-        f.write(' ')
-        for el in vec:
-            f.write(latt_form % el)
-        f.write('\n')
-    f.write('\n')
-
-    # Write atom positions in scaled or cartesian coordinates
-    if cartesian:
-        f.write('xangst\n')
-    else:
-        f.write('xred\n')
-
-    if long_format:
-        cform = ' %19.16f'
-    else:
-        cform = ' %9.6f'
-
-    for iatom, atom in enumerate(coord):
-        f.write(' ')
-        for dcoord in atom:
-            f.write(cform % dcoord)
-        f.write('\n')
-
-    if isinstance(filename, basestring):
-        f.close()
 
 
 def read_abinit_out(fd):
