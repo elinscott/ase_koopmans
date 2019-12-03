441d9010932e4e4745aa4fab5443c3d379b28298
Adam J. Jackson <a.j.jackson@physics.org>
2019-12-03 11:43:23 +0000

    TST: Fixed up XDATCAR roundtrip test
    - More tolerant Atoms comparison
    - Test now passes
    - No unittest related issues when test passes. The output on failure
      is a bit more confusing that necessary but does contain the
      necessary information and should be tolerable.

--- ase/test/fio/vasp_structure.py
+++ ase/test/fio/vasp_structure.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+import numpy.testing
+import ase
 import ase.build
 import ase.io
 
@@ -15,6 +17,32 @@ class Test_xdatcar_roundtrip(unittest.TestCase):
         if os.path.isfile(self.outfile):
             os.remove(self.outfile)
 
+    def assert_atoms_almost_equal(self, atoms, other):
+        """Compare two Atoms objects, raising AssertionError if different
+
+        This is quite redundant with the Atoms.__eq__ method, but more
+        tolerant of minor differences in the positions by a) comparing scaled
+        (i.e. wrapped) positions and b) using a comparison that tolerates small
+        errors.
+
+        """
+        self.assertIsInstance(atoms, ase.Atoms)
+        self.assertIsInstance(other, ase.Atoms)
+        self.assertEqual(len(atoms), len(other))
+        self.assertEqual(atoms.arrays['numbers'].tolist(),
+                         other.arrays['numbers'].tolist())
+        numpy.testing.assert_array_almost_equal(atoms.get_scaled_positions(),
+                                                other.get_scaled_positions(),
+                                                decimal=12)
+        numpy.testing.assert_array_almost_equal(atoms.cell, other.cell,
+                                                decimal=12)
+        self.assertTrue((atoms.pbc == other.pbc).all())
+
+    def assert_trajectory_almost_equal(self, traj1, traj2):
+        self.assertEqual(len(traj1), len(traj2))
+        for image, other in zip(traj1, traj2):
+            self.assert_atoms_almost_equal(image, other)
+
     def test_roundtrip(self):
         # Create a series of translated cells
         trajectory = [self.NaCl.copy() for i in range(5)]
@@ -24,11 +52,8 @@ class Test_xdatcar_roundtrip(unittest.TestCase):
             atoms.wrap()
 
         ase.io.write(self.outfile, trajectory, format='vasp-xdatcar')
-
         roundtrip_trajectory = ase.io.read(self.outfile, index=':')
-
-        self.assertEqual(roundtrip_trajectory, trajectory)
-
+        self.assert_trajectory_almost_equal(trajectory, roundtrip_trajectory)
 
 def suite():
     suite = unittest.defaultTestLoader.loadTestsFromTestCase(
