3cd871dd177e45d06f4bcd98f138e968ed49d7a0
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-12-23 14:23:45 +0100

    add new test suite module for managing pytest-style tests.  Rename some tests so they have (more) proper python module names

---
+++ ase/test/newtestsuite.py
@@ -0,0 +1,41 @@
+import runpy
+import unittest
+from pathlib import Path
+import ase.test as asetest
+from importlib.util import find_spec
+
+
+ignorefiles = {'__init__.py', 'testsuite.py', 'scripttests.py'}
+
+
+def get_test_modules():
+    testdir = Path(asetest.__file__).parent
+    testfiles = sorted(testdir.glob('*.py'))
+    testfiles += sorted(testdir.glob('*/*.py'))
+
+    modules = []
+    for testfile in testfiles:
+        if testfile.name in ignorefiles:
+            continue
+        rel_testfile = testfile.relative_to(testdir)
+        module = str(rel_testfile).rsplit('.', 1)[0].replace('/', '.')
+        modules.append('ase.test.{}'.format(module))
+    return modules
+
+
+def define_script_test_function(module):
+    assert module.startswith('ase.test.')
+    testname = module.split('.', 1)[1].replace('.', '_')
+    def test_script():
+        runpy.run_module(module, run_name='test')
+    test_script.__name__ = testname
+    return test_script
+
+
+def define_all_tests(namespace):
+    for module in get_test_modules():
+        testfunc = define_script_test_function(module)
+        namespace[testfunc.__name__] = testfunc
+
+
+define_all_tests(globals())
