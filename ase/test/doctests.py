45d46ff7ee0bff09499a25144bcb919f96e705bd
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-12-25 18:08:49 +0100

    use proper mechanism for numpy printing setup in doctests

--- ase/test/doctests.py
+++ ase/test/doctests.py
@@ -3,12 +3,21 @@ import importlib
 import shutil
 import sys
 from unittest import SkipTest, TestCase
+from distutils.version import LooseVersion
 
 import numpy as np
 
 if sys.version_info < (3, 6):
     raise SkipTest('Test requires Python 3.6+, this is {}'
                    .format(sys.version_info))
+
+
+# Older numpies format arrays differently.
+# We use the printoptions contextmanager from numpy 1.15:
+# https://docs.scipy.org/doc/numpy/release.html#id45
+if LooseVersion(np.__version__) < '1.15':
+    raise SkipTest('need numpy >= 1.15')
+
 
 module_names = """\
 ase.atoms
@@ -28,10 +37,12 @@ ase.spacegroup.xtal
 ase.symbols
 """
 
+
 for modname in module_names.splitlines():
     if modname == 'ase.spacegroup.findsym' and not shutil.which('findsym'):
         print('Skipping {} because we do not have findsym'.format(modname))
         continue
+
     mod = importlib.import_module(modname)
     with np.printoptions(legacy='1.13'):
         print(mod, doctest.testmod(mod, raise_on_error=True))
