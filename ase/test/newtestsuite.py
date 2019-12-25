59ffbe37b220ac601553b04a41982a8b65a258c4
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-12-25 18:08:04 +0100

    clean up a bit

--- ase/test/newtestsuite.py
+++ ase/test/newtestsuite.py
@@ -1,10 +1,22 @@
-"""Test suite.
+"""Old test suite expressed as pytest-type functions.
 
-Usage:
+This module exposes the old test suite as module-level test_xxx()
+functions recognized by pytest.
+
+Cheat sheet
+-----------
+
+Run tests:
 
   $ pytest newtestsuite.py
 
-This module """
+List tests:
+
+  $ pytest newtestsuite.py --collect-only
+
+We should start writing unittests as functions (or classes) that
+pytest works with.  We can also port the old tests to that form, where
+it matters."""
 
 
 import numpy as np
@@ -18,15 +30,35 @@ from ase.utils import workdir
 ignorefiles = {'__init__.py', 'testsuite.py', 'newtestsuite.py'}
 
 
+# Ignore calculator tests (for now):
+ignoredirs = {
+    'abinit', 'ace', 'aims', 'aims', 'amber',
+    'calculator', 'calculators',
+    'castep', 'cp2k', 'crystal', 'demon', 'demonnano',
+    'dftb', 'dmol', 'elk', 'espresso',
+    'exciting', 'fleur', 'gaussian', 'gpaw', 'gromacs', 'jacapo',
+    'kim', 'lammpslib', 'lammpsrun', 'nwchem',
+    'octopus', 'onetep', 'openmx', 'psi4',
+    'qbox', 'qchem', 'siesta', 'turbomole', 'vasp',
+}
+
+
 def find_all_test_modules() -> List[str]:
+    """Return a list of modules ['ase.test.xxx', 'ase.test.yyy', ...]."""
     testdir = Path(asetest.__file__).parent
     testfiles = sorted(testdir.glob('*.py'))
     testfiles += sorted(testdir.glob('*/*.py'))
+    # XXX Some tests were added at */*/*.py level, but the old test suite
+    # never globbed so deep.
 
     modules = []
     for testfile in testfiles:
         if testfile.name in ignorefiles:
             continue
+        if testfile.parent.name in ignoredirs:
+            continue
+        if '#' in testfile.name:
+            continue  # Ignore certain backup files
         rel_testfile = testfile.relative_to(testdir)
         module = str(rel_testfile).rsplit('.', 1)[0].replace('/', '.')
         modules.append('ase.test.{}'.format(module))
@@ -45,39 +77,17 @@ def define_script_test_function(module: str):
     return test_script
 
 
-disabled_normally = {'abinit',
-                     'ace', 'aims', 'aims', 'amber',
-                     'calculator',
-                     'calculators',
-                     'castep', 'cp2k', 'crystal',
-                     'demon', 'demonnano',
-                     'dftb', 'dmol', 'elk', 'espresso',
-                     'exciting', 'fleur',
-                     'gaussian', 'gpaw', 'gromacs', 'jacapo',
-                     'kim', 'lammpslib', 'lammpsrun', 'nwchem',
-                     'octopus', 'onetep', 'openmx', 'psi4',
-                     'qbox', 'qchem', 'siesta', 'turbomole', 'vasp'}
-
-
-def define_all_tests(namespace: Dict[str, Any]):
+def add_all_tests_to_namespace(namespace: Dict[str, Any]):
     for module in find_all_test_modules():
-        if '#' in module:  # Ignore certain backup files
-            continue
-
         if 'test_' in module or '_test' in module:
             raise RuntimeError('The old-style tests may not have the word '
                                '"_test" or "test_" in their name '
                                'lest they be confused with new-style tests: '
                                '{}'.format(module))
 
-        assert '-' not in module, module
-        tokens = module.split('.')
-        assert tokens[0] == 'ase'
-        assert tokens[1] == 'test'
-        if tokens[2] in disabled_normally:
-            continue
+        assert '-' not in module, module  # Rename/avoid improper module names
         testfunc = define_script_test_function(module)
         namespace[testfunc.__name__] = testfunc
 
 
-define_all_tests(globals())
+add_all_tests_to_namespace(globals())
