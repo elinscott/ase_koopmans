a7da495dad933f6575322e9e6cf3226b81ad1057
Morten Gjerding <mogje@fysik.dtu.dk>
2020-01-09 10:48:54 +0100

    Change special point text color to green

--- ase/dft/bz.py
+++ ase/dft/bz.py
@@ -155,16 +155,16 @@ def bz_plot(cell, vectors=False, paths=None, points=None,
                         name = '{}_{{{}}}'.format(name, num)
                 if dimensions == 3:
                     ax.text(x, y, z, '$' + name + '$',
-                            ha='center', va='bottom', color='k')
+                            ha='center', va='bottom', color='g')
                 elif dimensions == 2:
                     if abs(z) < 1e-6:
                         ax.text(x, y, '$' + name + '$',
-                                ha='center', va='bottom', color='k',
+                                ha='center', va='bottom', color='g',
                                 zorder=5)
                 else:
                     if abs(y) < 1e-6 and abs(z) < 1e-6:
                         ax.text(x, y, '$' + name + '$',
-                                ha='center', va='bottom', color='k',
+                                ha='center', va='bottom', color='g',
                                 zorder=5)
 
     if kpoints is not None:
