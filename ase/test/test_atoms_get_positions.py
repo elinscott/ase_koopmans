24827ff50799ff578ebc420182a40ab8012b6b89
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-02-07 23:16:01 +0100

    port get_positions test to pytest

--- ase/test/test_atoms_get_positions.py
+++ ase/test/test_atoms_get_positions.py
@@ -22,9 +22,3 @@ def test_positions_wrapped(atoms=atoms):
 def test_wrapped_positions(atoms=atoms):
     atoms.wrap()
     assert np.allclose(positions_wrapped, atoms.get_positions())
-
-
-if __name__ == "__main__":
-    test_positions()
-    test_positions_wrapped()
-    test_wrapped_positions()
