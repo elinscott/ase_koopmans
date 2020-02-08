6839ae613dad20f1b3bc389ef96d5885acc0049a
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-02-08 15:13:49 +0100

    port the doctests test

--- ase/test/test_doctests.py
+++ ase/test/test_doctests.py
@@ -1,21 +1,10 @@
 import doctest
 import importlib
 import sys
-from unittest import SkipTest
 from distutils.version import LooseVersion
 
+import pytest
 import numpy as np
-
-if sys.version_info < (3, 6):
-    raise SkipTest('Test requires Python 3.6+, this is {}'
-                   .format(sys.version_info))
-
-
-# Older numpies format arrays differently.
-# We use the printoptions contextmanager from numpy 1.15:
-# https://docs.scipy.org/doc/numpy/release.html#id45
-if LooseVersion(np.__version__) < '1.15':
-    raise SkipTest('need numpy >= 1.15')
 
 
 module_names = """\
@@ -34,10 +23,21 @@ ase.phasediagram
 ase.spacegroup.spacegroup
 ase.spacegroup.xtal
 ase.symbols
-"""
+""".split()
 
 
-for modname in module_names.splitlines():
+@pytest.mark.parametrize('modname', module_names)
+def test_doctest(modname):
+    if sys.version_info < (3, 6):
+        pytest.skip('Test requires Python 3.6+, this is {}'
+                    .format(sys.version_info))
+
+    # Older numpies format arrays differently.
+    # We use the printoptions contextmanager from numpy 1.15:
+    # https://docs.scipy.org/doc/numpy/release.html#id45
+    if LooseVersion(np.__version__) < '1.15':
+        pytest.skip('need numpy >= 1.15')
+
     mod = importlib.import_module(modname)
     with np.printoptions(legacy='1.13'):
         print(mod, doctest.testmod(mod, raise_on_error=True))
