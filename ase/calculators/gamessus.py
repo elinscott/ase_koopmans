f477a82b19c711ceb02a766033728ce43c143014
Eric Hermes <ehermes@sandia.gov>
2020-02-10 15:45:37 -0800

    Remove unnecessary set method

--- ase/calculators/gamessus.py
+++ ase/calculators/gamessus.py
@@ -5,15 +5,12 @@ from ase.calculators.calculator import FileIOCalculator
 class GAMESSUS(FileIOCalculator):
     implemented_properties = ['energy', 'forces', 'dipole']
     command = 'rungms PREFIX.inp > PREFIX.log'
+    discard_results_on_any_change = True
 
     def __init__(self, restart=None, ignore_bad_restart_file=False,
                  label='gamessus', atoms=None, command=None, **kwargs):
         FileIOCalculator.__init__(self, restart, ignore_bad_restart_file,
                                   label, atoms, command, **kwargs)
-
-    def set(self, **kwargs):
-        if FileIOCalculator.set(self, **kwargs):
-            self.reset()
 
     def write_input(self, atoms, properties=None, system_changes=None):
         FileIOCalculator.write_input(self, atoms, properties, system_changes)
