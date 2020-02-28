e4beb7a83bd83f13fe84b47d70077bd28cc58675
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-02-28 22:04:59 +0100

    port uncompletion test

--- ase/test/test_cell_uncompletion.py
+++ ase/test/test_cell_uncompletion.py
@@ -1,3 +1,4 @@
+import pytest
 import itertools
 from ase.cell import Cell
 
@@ -7,7 +8,8 @@ def all_pbcs():
     yield from itertools.product(values, values, values)
 
 
-def test(cell):
+@pytest.mark.parametrize('cell', Cell.new([3, 4, 5]), Cell.new([2, 0, 3]))
+def test_uncomplete(cell):
     for pbc in all_pbcs():
         ucell = cell.uncomplete(pbc)
         assert ucell.rank == sum(pbc & cell.any(1))
@@ -16,6 +18,3 @@ def test(cell):
     assert all(cell.uncomplete(1).any(1) == cell.any(1))
     assert cell.uncomplete(False).rank == 0
     assert cell.uncomplete(0).rank == 0
-
-test(Cell.new([3, 4, 5]))
-test(Cell.new([2, 0, 3]))
