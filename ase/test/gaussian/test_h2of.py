0d4203ece2923c61894c3d3e861852548596d268
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-02-28 23:26:24 +0100

    port h2of gaussian test

--- ase/test/gaussian/test_h2of.py
+++ ase/test/gaussian/test_h2of.py
@@ -53,25 +53,27 @@ D   1   1.00
 ****
 """
 
-with open('def2-svp.gbs', 'w') as bfile:
-    bfile.write(basis)
 
-atoms = Atoms('OH2F', positions=[(-1.853788, -0.071113, 0.000000),
-                                 (-1.892204, 0.888768, 0.000000),
-                                 (-0.888854, -0.232973, 0.000000),
-                                 (1.765870, 0.148285, 0.000000)])
+def test_h2of():
+    with open('def2-svp.gbs', 'w') as bfile:
+        bfile.write(basis)
 
-label = 'h2of-anion'
-calc = Gaussian(charge=-1.0,
-                basis='gen',
-                method='B3LYP',
-                basisfile='@def2-svp.gbs/N',
-                label=label,
-                ioplist=['6/80=1', '6/35=4000000'],
-                density='current',
-                addsec=['%s.wfx' % label]
-               )
+    atoms = Atoms('OH2F', positions=[(-1.853788, -0.071113, 0.000000),
+                                     (-1.892204, 0.888768, 0.000000),
+                                     (-0.888854, -0.232973, 0.000000),
+                                     (1.765870, 0.148285, 0.000000)])
 
-atoms.set_calculator(calc)
-atoms.get_potential_energy()
+    label = 'h2of-anion'
+    calc = Gaussian(charge=-1.0,
+                    basis='gen',
+                    method='B3LYP',
+                    basisfile='@def2-svp.gbs/N',
+                    label=label,
+                    ioplist=['6/80=1', '6/35=4000000'],
+                    density='current',
+                    addsec=['%s.wfx' % label]
+                   )
+
+    atoms.set_calculator(calc)
+    atoms.get_potential_energy()
 
