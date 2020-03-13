e90efc9b1721ee78f9a3172a86351127c3ffc6b3
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-03-13 20:09:03 +0100

    reduce duplication

--- ase/test/espresso/test_espresso.py
+++ ase/test/espresso/test_espresso.py
@@ -4,15 +4,28 @@ from ase.build import bulk
 from ase.calculators.espresso import Espresso
 
 
-def test_main(espresso_factory):
-    silicon = bulk('Si')
-    calc = espresso_factory.calc()
-    silicon.calc = calc
-    print(silicon.calc.parameters)
-    silicon.get_potential_energy()
-
+def verify(calc):
     assert calc.get_fermi_level() is not None
     assert calc.get_ibz_k_points() is not None
     assert calc.get_eigenvalues(spin=0, kpt=0) is not None
     assert calc.get_number_of_spins() is not None
     assert calc.get_k_point_weights() is not None
+
+
+def test_main(espresso_factory):
+    atoms = bulk('Si')
+    calc = espresso_factory.calc()
+    atoms.calc = calc
+    atoms.get_potential_energy()
+    verify(calc)
+
+
+def test_smearing(espresso_factory):
+    atoms = bulk('Cu')
+    input_data = {'system':{'occupations': 'smearing',
+                            'smearing': 'fermi-dirac',
+                            'degauss': 0.02}}
+    calc = espresso_factory.calc(input_data=input_data)
+    atoms.set_calculator(calc)
+    atoms.get_potential_energy()
+    verify(calc)
