3e665cf1586ef9d802752e1a33dcde6f21179a24
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-03-14 16:03:26 +0100

    more work on calculator factories.  Allow @pytest.mark.calculator([abinit, ...]) to parametrize over calculators

--- ase/test/conftest.py
+++ ase/test/conftest.py
@@ -4,14 +4,23 @@ from ase.utils import workdir
 from pathlib import Path
 
 
+@pytest.fixture(scope='session')
+def enabled_calculators():
+    from ase.calculators.calculator import names as calculator_names
+    all_names = set(calculator_names)
+    names = set(os.environ.get('ASE_TEST_CALCULATORS', '').split())
+    for name in names:
+        assert name in all_names
+    return names
+
+
 @pytest.fixture(scope='session', autouse=True)
-def disable_calculators(request):
+def monkeypatch_disabled_calculators(request, enabled_calculators):
     from ase.test.testsuite import disable_calculators, test_calculator_names
     from ase.calculators.calculator import names as calculator_names
-    enabled_names = os.environ.get('ASE_TEST_CALCULATORS', '').split()
-    test_calculator_names += enabled_names
+    test_calculator_names += enabled_calculators
     disable_calculators([name for name in calculator_names
-                         if name not in enabled_names])
+                         if name not in enabled_calculators])
 
 
 # Backport of tmp_path fixture from pytest 3.9.
@@ -64,8 +73,12 @@ def psycopg2():
 
 @pytest.fixture(scope='session')
 def datafiles():
-    asetest = pytest.importorskip('asetest')
-    return asetest.datafiles
+    try:
+        import asetest
+    except ImportError:
+        return {}
+    else:
+        return asetest.datafiles.paths
 
 
 @pytest.fixture(scope='session')
@@ -75,13 +88,25 @@ def executables():
     if path is None:
         path = Path.home() / '.ase' / 'executables.json'
     path = Path(path)
-    if not path.exists():
-        pytest.skip('Environment variable ASE_EXECUTABLE_CONFIG not set '
-                    'and {} does not exist'.format(path))
-    dct = json.loads(path.read_text())
+    if path.exists():
+        dct = json.loads(path.read_text())
+    else:
+        dct = {}
     return dct
 
 
+factory_classes = {}
+
+
+def factory(name):
+    def deco(cls):
+        cls.name = name
+        factory_classes[name] = cls
+        return cls
+    return deco
+
+
+@factory('abinit')
 class AbinitFactory:
     def __init__(self, executable, pp_paths):
         self.executable = executable
@@ -101,7 +126,13 @@ class AbinitFactory:
         kw.update(kwargs)
         return Abinit(**kw)
 
+    @classmethod
+    def fromconfig(cls, config):
+        return AbinitFactory(config.executables['abinit'],
+                             config.datafiles['abinit'])
 
+
+@factory('cp2k')
 class CP2KFactory:
     def __init__(self, executable):
         self.executable = executable
@@ -110,7 +141,12 @@ class CP2KFactory:
         from ase.calculators.cp2k import CP2K
         return CP2K(command=self.executable, **kwargs)
 
+    @classmethod
+    def fromconfig(cls, config):
+        return CP2KFactory(config.executables['cp2k'])
 
+
+@factory('espresso')
 class EspressoFactory:
     def __init__(self, executable, pseudo_dir):
         self.executable = executable
@@ -136,7 +172,14 @@ class EspressoFactory:
                         pseudopotentials=pseudopotentials,
                         **kw)
 
+    @classmethod
+    def fromconfig(cls, config):
+        paths = config.datafiles['espresso']
+        assert len(paths) == 1
+        return cls(config.executables['espresso'], paths[0])
 
+
+@factory('siesta')
 class SiestaFactory:
     def __init__(self, executable, pseudo_path):
         self.executable = executable
@@ -148,27 +191,66 @@ class SiestaFactory:
         return Siesta(command=command, pseudo_path=str(self.pseudo_path),
                       **kwargs)
 
+    @classmethod
+    def fromconfig(cls, config):
+        paths = config.datafiles['siesta']
+        assert len(paths) == 1
+        path = paths[0]
+        return cls(config.executables['siesta'], str(path))
+
+
+class ExternalCodes:
+    def __init__(self, executables, datafiles):
+        self.executables = executables
+        self.datafiles = datafiles
+        self._factories = {}
+
+    def __getitem__(self, name):
+        # Hmm.  We could also just return a new factory instead of
+        # caching them.
+        if name not in self._factories:
+            cls = factory_classes[name]
+            try:
+                factory = cls.fromconfig(self)
+            except KeyError:
+                pytest.skip('Missing configuration for {}'.format(name))
+            self._factories[name] = factory
+        return self._factories[name]
+
 
 @pytest.fixture(scope='session')
-def abinit_factory(executables, datafiles):
-    return AbinitFactory(executables['abinit'],
-                         datafiles.paths['abinit'])
+def factories(executables, datafiles, enabled_calculators):
+    return ExternalCodes(executables, datafiles)
 
 
-@pytest.fixture(scope='session')
-def cp2k_factory(executables):
-    return CP2KFactory(executables['cp2k'])
+def newfactory(name):
+    @pytest.fixture(scope='session')
+    def factory(factories):
+        return factories[name]
+    factory.__name__ = '{}_factory'.format(name)
+    return factory
 
 
-@pytest.fixture(scope='session')
-def espresso_factory(executables, datafiles):
-    paths = datafiles.paths['espresso']
-    assert len(paths) == 1
-    return EspressoFactory(executables['espresso'], paths[0])
+abinit_factory = newfactory('abinit')
+cp2k_factory = newfactory('cp2k')
+espresso_factory = newfactory('espresso')
+siesta_factory = newfactory('siesta')
 
 
-@pytest.fixture(scope='session')
-def siesta_factory(executables, datafiles):
-    paths = datafiles.paths['siesta']
-    assert len(paths) == 1
-    return SiestaFactory(executables['siesta'], paths[0])
+@pytest.fixture
+def code(request, factories):
+    return factories[request.param]
+
+
+def pytest_generate_tests(metafunc):
+    """Parametrize tests using our custom markers.
+
+    We want tests marked with @pytest.mark.calculator(names) to be
+    parametrized over the named calculator or calculators."""
+    # Is it okay to use metafunc.definition.iter_markers()?
+    # Or is that private or something?
+    for marker in metafunc.definition.iter_markers(name='calculator'):
+        calculator_names = marker.args[0]
+        if isinstance(calculator_names, str):
+            calculator_names = [calculator_names]
+        metafunc.parametrize('code', calculator_names, indirect=True)
