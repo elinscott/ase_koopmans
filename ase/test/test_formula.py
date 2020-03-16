382bd72927dbd2b4248ed2f9b36409891b500a48
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-03-16 08:50:08 +0100

    Test result

--- ase/test/test_formula.py
+++ ase/test/test_formula.py
@@ -7,7 +7,7 @@ from ase.formula import Formula
 
 def test_h0c1():
     f = Formula.from_dict({'H': 0, 'C': 1})
-    print(f.format('hill'))
+    assert f.format('hill') == 'C'
     with pytest.raises(ValueError):
         Formula.from_dict({'H': -1})
     with pytest.raises(ValueError):
