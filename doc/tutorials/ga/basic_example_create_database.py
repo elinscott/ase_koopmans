a4eb26c2d69632f32a3f7afe26ffbb95fd643b7f
Maxime Van den Bossche <maxime.cp.vandenbossche@gmail.com>
2020-03-19 18:42:53 +0000

    Apply suggestion to doc/tutorials/ga/basic_example_create_database.py

--- doc/tutorials/ga/basic_example_create_database.py
+++ doc/tutorials/ga/basic_example_create_database.py
@@ -28,7 +28,7 @@ atom_numbers = 2 * [47] + 2 * [79]
 
 # define the closest distance two atoms of a given species can be to each other
 unique_atom_types = get_all_atom_types(slab, atom_numbers)
-cd = closest_distances_generator(atom_numbers=unique_atom_types,
+blmin = closest_distances_generator(atom_numbers=unique_atom_types,
                                  ratio_of_covalent_radii=0.7)
 
 # create the starting population
