04dd7bf1a0ec2ec5795cd62f4f8f5d229df441a8
Florian Knoop <florian_knoop@gmx.de>
2020-04-01 13:16:32 +0200

    FIX: `LennardJones(Calculator)` when `'numbers' not in system_changes`

--- ase/test/calculators/test_lj.py
+++ ase/test/calculators/test_lj.py
@@ -1,9 +1,15 @@
 from ase import Atoms
 from ase.calculators.lj import LennardJones
 
+atoms = Atoms("H2", positions=[[0, 0, 0], [0, 0, 2 ** (1.0 / 6.0)]])
+
 
 def test_minimum_energy():
     # See https://en.wikipedia.org/wiki/Lennard-Jones_potential
-    atoms = Atoms('H2', positions=[[0, 0, 0], [0, 0, 2**(1. / 6.)]])
-    atoms.set_calculator(LennardJones(rc=1.e5))
+    atoms.set_calculator(LennardJones(rc=1.0e5))
     assert atoms.get_potential_energy() == -1.0
+
+
+def test_system_changes():
+    atoms.set_calculator(LennardJones(rc=1.0e5))
+    atoms.calc.calculate(atoms, system_changes=["positions"])
