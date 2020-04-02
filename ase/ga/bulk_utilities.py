24f443a270ddeb5e7187e5362a2d310eb81e3f21
mvdb <maxime.cp.vandenbossche@gmail.com>
2020-04-02 17:01:41 -0400

    DEP: add module files with deprecation warnings

---
+++ ase/ga/bulk_utilities.py
@@ -0,0 +1,4 @@
+raise DeprecationWarning(
+        'The ase.ga.bulk_utilities module has been deprecated. '
+        'The same functionality is now provided by the '
+        'ase.ga.utilities module.')
