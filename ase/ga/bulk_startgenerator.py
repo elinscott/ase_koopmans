9b09e28df943d129bfc92a99fe184c24e168552e
mvdb <maxime.cp.vandenbossche@gmail.com>
2020-04-04 07:58:15 -0400

    STY: remove trailing white space and blank line

--- ase/ga/bulk_startgenerator.py
+++ ase/ga/bulk_startgenerator.py
@@ -2,5 +2,4 @@ raise DeprecationWarning(
         'The ase.ga.bulk_startgenerator module has been deprecated. '
         'The same functionality is now provided by the '
         'ase.ga.startgenerator module. Please consult its documentation '
-        'to verify how to e.g. initialize a StartGenerator object.') 
-
+        'to verify how to e.g. initialize a StartGenerator object.')
