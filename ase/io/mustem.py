Ran autopep8 and fix comparison precision of oldlibs test.

ase/io/mustem.py
@@ -69,7 +69,7 @@ class XtlmuSTEMWriter:
         if not cell.orthorhombic:
             raise ValueError('To export to this format, the cell needs to be '
                              'orthorhombic.')
-        if (cell.diagonal() ==  0).any():
+        if (cell.diagonal() == 0).any():
             raise ValueError('To export to this format, the cell size needs '
                              'to be set: current cell is {}.'.format(cell))
         self.atoms = atoms.copy()
@@ -86,7 +86,7 @@ class XtlmuSTEMWriter:
 
     def _get_occupancy(self, occupancy):
         if np.isscalar(occupancy):
-            occupancy = {atom:occupancy for atom in self.atom_types}
+            occupancy = {atom: occupancy for atom in self.atom_types}
         elif isinstance(occupancy, dict):
             self._check_key_dictionary(occupancy, 'occupancy')
 

ase/io/prismatic.py
@@ -49,7 +49,6 @@ def read_prismatic(filename):
     # Use genfromtxt instead of loadtxt to skip last line
     read_data = np.genfromtxt(fname=filename, skip_footer=1)
 
-
     atoms = Atoms(symbols=read_data[:, 0],
                   positions=read_data[:, 1:4],
                   cell=cellpar_to_cell(cellpar),
@@ -69,7 +68,7 @@ class XYZPrismaticWriter:
         if not cell.orthorhombic:
             raise ValueError('To export to this format, the cell needs to be '
                              'orthorhombic.')
-        if (cell.diagonal() ==  0).any():
+        if (cell.diagonal() == 0).any():
             raise ValueError('To export to this format, the cell size needs '
                              'to be set: current cell is {}.'.format(cell))
         self.atoms = atoms.copy()
@@ -98,7 +97,7 @@ class XYZPrismaticWriter:
         elif isinstance(DW, dict):
             self._check_key_dictionary(DW, 'DW')
             # Get the arrays of DW from mapping the DW defined by symbol
-            DW = {symbols2numbers(k)[0]:v for k, v in DW.items()}
+            DW = {symbols2numbers(k)[0]: v for k, v in DW.items()}
             DW = np.vectorize(DW.get)(self.atoms.numbers)
         else:
             for name in ['DW', 'debye_waller_factor']:
@@ -113,7 +112,6 @@ class XYZPrismaticWriter:
                              'the `Atoms` object.')
 
         return DW
-
 
     def _check_key_dictionary(self, d, dict_name):
         # Check if we have enough key

ase/test/fio/test_mustem.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pytest
+
 from ase import Atoms
 from ase.io import read
 
@@ -34,8 +36,9 @@ def test_mustem():
     atoms3 = read(filename)
 
     for _atoms in [atoms2, atoms3]:
-        assert atoms.positions.sum() - _atoms.positions.sum() == 0
-        assert atoms.cell.sum() - _atoms.cell.sum() == 0
+        np.testing.assert_allclose(atoms.positions.sum(),
+                                   _atoms.positions.sum())
+        np.testing.assert_allclose(atoms.cell.sum(), _atoms.cell.sum())
 
     with pytest.raises(ValueError):
         # Raise an error if there is a missing key.
@@ -63,7 +66,7 @@ def test_mustem():
                      DW={'Sr': 0.78700E-02, 'O': 0.92750E-02, 'Ti': 0.55700E-02})
 
     # Setting Debye-Waller factor as float.
-    Si_atoms = Atoms(symbols='Si'*8,
+    Si_atoms = Atoms(symbols='Si' * 8,
                      positions=[[0.0000, 0.0000, 0.0000],
                                 [2.7150, 2.7150, 0.0000],
                                 [1.3575, 4.0725, 1.3575],
@@ -79,5 +82,6 @@ def test_mustem():
     Si_atoms.write(filename, keV=300, DW=0.78700E-02)
     Si_atoms2 = read(filename)
 
-    assert Si_atoms.positions.sum() - Si_atoms2.positions.sum() == 0
-    assert Si_atoms.cell.sum() - Si_atoms2.cell.sum() == 0
+    np.testing.assert_allclose(Si_atoms.positions.sum(),
+                               Si_atoms2.positions.sum())
+    np.testing.assert_allclose(Si_atoms.cell.sum(), Si_atoms2.cell.sum())

ase/test/fio/test_prismatic.py
@@ -18,7 +18,7 @@ pytestmark = pytest.mark.skipif(LooseVersion(np.__version__) <
 
 def make_Si100_atoms():
     # Reproduce the SI100.XYZ file distributed with prismatic
-    atoms = Atoms(symbols='Si'*8,
+    atoms = Atoms(symbols='Si' * 8,
                   positions=[[0.0000, 0.0000, 0.0000],
                              [2.7150, 2.7150, 0.0000],
                              [1.3575, 4.0725, 1.3575],
@@ -51,7 +51,7 @@ def test_write_read_cycle_xyz_prismatic():
     # Reproduce the SI100.XYZ file distributed with prismatic
     atoms = make_Si100_atoms()
     atoms.set_array('occupancy', np.ones_like(atoms.numbers))
-    atoms.set_array('debye_waller_factor', np.ones_like(atoms.numbers)*0.076)
+    atoms.set_array('debye_waller_factor', np.ones_like(atoms.numbers) * 0.076)
 
     filename = os.path.join(tmpDir, 'SI100.XYZ')
     atoms.write(filename=filename, format='prismatic',
@@ -81,7 +81,7 @@ def test_write_error():
     atoms_Si100.write(filename, format='prismatic', DW=0.076)
 
     # Write file with DW provided as dict
-    atoms_Si100.write(filename, format='prismatic', DW={'Si':0.076})
+    atoms_Si100.write(filename, format='prismatic', DW={'Si': 0.076})
 
     with pytest.raises(ValueError):
         # DW missing keys