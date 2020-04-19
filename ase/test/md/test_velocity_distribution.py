f04168e05ac2f2f99fe453c14e44278b895f4116
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-04-19 15:59:09 +0200

    flake

--- ase/test/md/test_velocity_distribution.py
+++ ase/test/md/test_velocity_distribution.py
@@ -31,6 +31,7 @@ def propagate(atoms):
     atoms.positions += displacement
     return atoms
 
+
 def test_stationary(atoms, stationary_atoms):
     assert norm(atoms.get_momenta().sum(axis=0)) > 0.1
     assert norm(stationary_atoms.get_momenta().sum(axis=0)) < 1e-13
