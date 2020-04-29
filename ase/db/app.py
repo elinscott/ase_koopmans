0c6cd62ec4ec2871fbfd0d2e59daf0fb093d6c4f
Jens Jørgen Mortensen <jj@smoerhul.dk>
2020-04-29 16:33:33 +0200

    Show how to install jsmol

--- ase/db/app.py
+++ ase/db/app.py
@@ -43,6 +43,23 @@ app = Flask(__name__, template_folder=str(root))
 projects = {}  # type: Dict[str, Dict[str, Any]]
 
 
+static = root / 'ase/db/static'
+if not (static / 'jsmol/JSmol.min.js').is_file():
+    print(f"""
+WARNING:
+    You don't have jsmol on your system.
+
+    Download Jmol-*-binary.tar.gz from
+    https://sourceforge.net/projects/jmol/files/Jmol/,
+    extract jsmol.zip, unzip it and create a soft-link:
+
+        $ tar -xf Jmol-*-binary.tar.gz
+        $ unzip jmol-*/jsmol.zip
+        $ ln -s $PWD/jsmol {static}/jsmol
+""",
+          file=sys.stderr)
+
+
 @app.route('/', defaults={'project_name': 'default'})
 @app.route('/<project_name>')
 @app.route('/<project_name>/')
