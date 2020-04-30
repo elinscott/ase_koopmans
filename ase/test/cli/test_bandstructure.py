05d3c01af3be94780312f02e3814eeebae8d1356
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-04-30 22:11:12 +0200

    depend on plt fixture because of dependency

--- ase/test/cli/test_bandstructure.py
+++ ase/test/cli/test_bandstructure.py
@@ -2,7 +2,7 @@ from ase.lattice import RHL
 from pathlib import Path
 
 
-def test_ase_bandstructure(cli):
+def test_ase_bandstructure(cli, plt):
     lat = RHL(3., 70.0)
     path = lat.bandpath()
     bs = path.free_electron_band_structure()
