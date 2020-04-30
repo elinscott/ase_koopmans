49c66d32db9838e6093dcea4fc12df29d4e1db03
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-04-30 21:27:07 +0200

    at least assert that expected output was written

--- ase/test/cli/test_bandstructure.py
+++ ase/test/cli/test_bandstructure.py
@@ -1,15 +1,20 @@
 from ase.lattice import RHL
+from pathlib import Path
 
 
 def test_ase_bandstructure(cli):
     lat = RHL(3., 70.0)
     path = lat.bandpath()
     bs = path.free_electron_band_structure()
-    bs.write('bs.json')
 
-    cli.ase('band-structure bs.json --output bs.png')
+    bs_path = Path('bs.json')
+    bs.write(bs_path)
+
+    fig_path = Path('bs.png')
+
+    cli.ase(f'band-structure {bs_path} --output {fig_path}')
     # If the CLI tool gave a text output, we could verify it.
-
+    assert fig_path.is_file()
 
 # Note: We don't have proper testing of --points, --range etc.  We
 # test here on JSON input but the tool is in principle supposed to
