8add0f40c8f1b3d2f2fd1a2b42fe4bfd4440ab2e
Edward Linscott <edwardlinscott@gmail.com>
2020-12-14 16:36:28 +0100

    Resolving bug in previous commit (#11)

--- ase/io/espresso_cp.py
+++ ase/io/espresso_cp.py
@@ -76,8 +76,43 @@ _CP_LAMBDA = 'fixed_lambda'
 def write_espresso_cp_in(fd, atoms, input_data=None, pseudopotentials=None,
                       kspacing=None, kpts=None, koffset=(0, 0, 0),
                       **kwargs):
+
     write_espresso_in(fd, atoms, input_data, pseudopotentials,
                       kspacing, kpts, koffset, **kwargs)
+
+    if not fd.closed:
+        fd.close()
+    
+    # Extra blocks
+    extra_lines = []
+    input_parameters = construct_namelist(input_data, **kwargs)
+    for section in input_parameters:
+        if section.lower() not in ['ee', 'nksic']:
+            continue
+        extra_lines.append('&{0}\n'.format(section.upper()))
+        for key, value in input_parameters[section].items():
+            if value is True:
+                extra_lines.append('   {0:16} = .true.\n'.format(key))
+            elif value is False:
+                extra_lines.append('   {0:16} = .false.\n'.format(key))
+            elif value is not None:
+                # repr format to get quotes around strings
+                extra_lines.append('   {0:16} = {1!r:}\n'.format(key, value))
+        extra_lines.append('/\n')  # terminate section
+    extra_lines.append('\n')
+
+    # Read in the original file without the NKSIC and EE blocks
+    with open(fd.name, 'r') as fd_read:
+        lines = fd_read.readlines()
+
+    # Find where to insert the extra blocks
+    i_break = lines.index('\n')
+    before = lines[:i_break]
+    after = lines[i_break:]
+
+    # Rewrite the file with the extra blocks
+    with open(fd.name, 'w') as fd_rewrite:
+        fd_rewrite.writelines(before + extra_lines + after)
 
 
 def read_espresso_cp_in(fileobj):
