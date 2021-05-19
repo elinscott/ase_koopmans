a0ebaa49b5368a9d9192a2a9d03a6c8616830e38
Edward Linscott <edwardlinscott@gmail.com>
2021-05-19 21:26:21 +0200

    Shifted koopmans_ham DOS to be aligned to the VBM

--- ase/calculators/espresso/_koopmans_ham.py
+++ ase/calculators/espresso/_koopmans_ham.py
@@ -35,6 +35,17 @@ class KoopmansHam(EspressoParent):
         # Construct bandstructure here (rather than within self.calculate()) because we have access to the band path
         assert 'energies' in self.results, 'Please call {0}.calculate() prior to calling {0}.band_structure'.format(
             self.__class__.__name__)
+
         energies_np = np.array([self.results['energies']])
+
+        # Shift so that the VBM = 0.0
+        try:
+            num_occ = self.parameters['input_data']['wannier']['num_wann_occ']
+            if num_occ > 0:
+                reference = np.max(energies_np[:, :, num_occ - 1])
+                energies_np -= reference
+        except KeyError():
+            pass
+
         self.results['band structure'] = BandStructure(self.parameters['kpts'], energies_np)
         return self.results['band structure']
