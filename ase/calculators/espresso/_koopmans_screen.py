68684b214dcdf782abac1b8b109ed68663597558
Riccardo De Gennaro <riccardo.degennar@gmail.com>
2022-03-09 13:11:34 +0100

    Compliance with new format of KCW (#37)
    * updating parse of magnetic moments in pw
    
    * kc_ham kc_screen and wann2kc are now modes of kcw
    
    * Better parsing of pw magmom
    
    Co-authored-by: Edward Linscott <edwardlinscott@gmail.com>

--- ase/calculators/espresso/_koopmans_screen.py
+++ ase/calculators/espresso/_koopmans_screen.py
@@ -1,10 +1,10 @@
-""" Calculator for koopmans_screen.x, a code for performing Koopmans calculations in periodic systems
+""" Calculator for kcw.x (screen mode), a code for performing Koopmans calculations in periodic systems
 
-export ASE_KOOPMANS_HAM_COMMAND="/path/to/kc_screen.x -in PREFIX.khi > PREFIX.kho"
+export ASE_KOOPMANS_HAM_COMMAND="/path/to/kcw.x -in PREFIX.ksi > PREFIX.kso"
 
 N.B. the extensions must be .ksi and .kso
 
-Run kc_screen.x jobs.
+Run kcw.x jobs in the 'screen' mode.
 """
 
 
@@ -16,8 +16,8 @@ class KoopmansScreen(EspressoParent):
     ext_out = '.kso'
     implemented_properties = []
 
-    # Default command does not use parallelism and assumes kc_screen.x is on the user's path
-    command = 'kc_screen.x -in PREFIX.ksi > PREFIX.kso'
+    # Default command does not use parallelism and assumes kcw.x is on the user's path
+    command = 'kcw.x -in PREFIX.ksi > PREFIX.kso'
 
     def __init__(self, *args, **kwargs):
         kwargs['label'] = 'kc_screen'
