1a4cf9ceec626d0b6b08d8899dbfb06abb4429da
Yannick Schubert <schuyann@student.ethz.ch>
2022-05-09 14:06:17 +0200

    This should correct the extraction of the centers from a wannier calculation

--- ase/io/wannier90.py
+++ ase/io/wannier90.py
@@ -430,7 +430,7 @@ def read_wannier90_out(fd):
             j = 1
             while 'WF centre and spread' in flines[i + j]:
                 splitline = [x.rstrip(',') for x in re.sub('[(),]', ' ', flines[i + j]).split()]
-                centers.append([float(x) for x in splitline[6:9]])
+                centers.append([float(x) for x in splitline[5:8]])
                 spreads.append(float(splitline[-1]))
                 j += 1
 
