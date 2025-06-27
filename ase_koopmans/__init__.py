4a778f8979a8f9cb55601b8432379465bc6d16bd
Edward Linscott <edwardlinscott@gmail.com>
2025-06-27 12:22:23 +0200

    Update version

--- ase_koopmans/__init__.py
+++ ase_koopmans/__init__.py
@@ -19,7 +19,7 @@ if Version(np.__version__) < Version('1.9'):
 
 __all__ = ['Atoms', 'Atom']
 
-__version__ = '0.1.10'
+__version__ = '0.1.11'
 
 # import ase_koopmans.parallel early to avoid circular import problems when
 # ase_koopmans.parallel does "from gpaw.mpi import world":
